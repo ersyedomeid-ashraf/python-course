db8d606d4851b5f0367c958e450e7b70a0c1a1c6
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-05-23 06:24:54 +0530

    new code

---
+++ Basics/tut2comments.py
@@ -0,0 +1,21 @@
+#I am learning comments
+print("This is Code and Debug") #This is a print statement 
+print("I am Learning comments") #This is another print statement
+
+#This is single-line comments.
+# print("Hello World")
+# print("This is print")
+# print("I am learnign commets")
+
+""" 
+Hello everyone 
+Hi, I'm Syed Omeid Ashraf
+I am learning python
+"""
+
+''''
+hello everyone 
+I'm learning single-line comments 
+or 
+I'm learning multi-line comments
+'''
