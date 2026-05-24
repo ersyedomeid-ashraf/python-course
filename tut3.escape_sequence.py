6b15d4c9a611abe4bc6d629709650c8d9ffa4400
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-05-24 07:17:27 +0530

    added new program

---
+++ tut3.escape_sequence.py
@@ -0,0 +1,38 @@
+print("Hello World")
+print("how are you")
+print("I love shayari")
+
+# "\n is a New line escape sequence."
+
+print("Hello World \nThis is Code and Debug") 
+print("My name is Syed Omeid Ashraf\nMy age is 20 Years\nMy nGender is Male")
+print("Tune hiii apne kadam peeche kar liye\nMujh main kahan himmat thi tjhe gawane kii\nTere liye toh me harr shakhs ko thukra deta\nTu zidd tioh krta mjhe paane kii")
+
+
+# "\t is a Tab escape sequnce."
+
+print("This is \t\t\tPython Code")
+print("I'm Syed\tOmeid\tAshraf")
+print("I live in \t\tBareilly")
+
+
+# \" is a Double Quotation escape sequence.
+
+print("My name is 'Syed Omeid Ashraf'")
+print("I write 'Shayari")
+print("I live in 'Bareilly'")
+
+# \' is a Single Quotation escape sequence.
+
+print('I am pursuing "BCA"')
+print('I live in "Uttar Pradesh"')
+# Or we'll write in this form also
+print("I love \"BMW\"")
+print('Ilove \'BMW M5 Competetion\'')
+
+
+# "\\ is a Backslash escape sequence."
+#If i want this " Syed\Omeid Ashraf " in my output
+
+print("My name is Syed\\Omeid Ashraf")
+print("How are\\you")
