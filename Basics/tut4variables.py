daa5a91ee965702004839e12f54e07e529dc67f0
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-05-25 16:51:05 +0530

    Add new file

---
+++ Basics/tut4variables.py
@@ -0,0 +1,32 @@
+x = 23
+y = 13
+z = x+y
+
+print(x)
+print(y)
+print(z)
+
+name = "Syed Omeid Ashraf"
+age = 20
+gender = "Male"
+city = "Bareilly"
+
+print(name)
+print(age)
+print(gender)
+print(city) 
+
+__=39
+____=48
+_____=11
+print(__+____+_____)
+
+firstname = "Syed"
+print(firstname)
+
+# you can write in this form also 
+
+first_name = "Syed"
+mid_name = "Omeid"
+last_name = "Ashraf"
+print(first_name,mid_name,last_name)
---
+++ Basics/tut5printing_variable.py
@@ -0,0 +1,18 @@
+name = "Syed Omeid Ashraf"
+age = 20
+gender = "Male"
+
+#Now Printing Variables in different ways
+
+#Method 1 
+print("My name is",name)
+print("My age is",age) 
+print("My gender is",gender)
+print("My name is",name,"My age is",age,"My gender is",gender)
+
+#Method 2 (F Strings) Writing "F" is necessary. and It was introduced by "Python Developers"
+
+print(f"My name is {name}")       #We write it inside "Curly Braces"
+print(f"My age is {age}")
+print(f"My gender is {gender}")
+print(f"My name is {name} My age is {age} My gender is {gender}") 
