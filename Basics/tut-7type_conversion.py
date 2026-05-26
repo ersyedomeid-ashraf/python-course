498051c77af9219ea997d98feb14e5a76f8e0885
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-05-26 07:46:17 +0530

    Add new file

---
+++ Basics/tut-7type_conversion.py
@@ -0,0 +1,43 @@
+x = "BMW M5"
+y = "Competetion"
+z = x+y
+print(z)
+
+x = "80"
+y = "77"
+# Converting string values into integer values
+a = int(x)
+b = int(y)
+
+z = (a+b)
+print(z)
+
+
+a = "102.8"
+b = "234.7"
+# Converting string values into float values
+x = float(a)
+y = float(b)
+
+z = (x+y)
+print(z)
+
+
+c = 23
+d = 46
+# Coverting integer values intop string values
+x = str(c)
+y = str(d)
+
+z = (x+y)
+print(z)
+
+
+e = 45
+f = 67
+# Converting integer values into float values 
+x = float(e)
+y = float(f)
+
+z = (x+y)
+print(z)
---
+++ Basics/tut6-Data_types.py
@@ -0,0 +1,32 @@
+# int = integer values
+x = 65
+print(x)
+print(type(x))                              
+
+# int = integer values
+x = 72
+print(x,type(x))
+y = 23.4
+
+# float = decimal values
+print(y,type(y))
+
+# str = string/text values
+name = "Syed"
+print(name,type(name)) 
+
+# bool = True or False values 
+adult = True
+print(adult,type(adult))
+
+# list = multiple values inside square brackets []
+marks = [33,44,78,80,77]
+print(marks,type(marks))
+
+# tuple = multiple values inside parenthesis ()
+marks = (80,77,50,65,36)
+print(marks,type(marks))
+
+# dictionary = key-value pairs inside curly braces {}
+marks = {"Syed":70,"Ayush":35,"Ambani":19}
+print(marks,type(marks))
