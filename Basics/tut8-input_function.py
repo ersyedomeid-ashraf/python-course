2dcf9bdfd3d8c8dccdb2519314726b2e207fac18
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-05-27 15:04:53 +0530

    Add new file

---
+++ Basics/tut8-input_function.py
@@ -0,0 +1,19 @@
+name = "Syed"
+age = 20
+
+print(f"your name is {name}")
+print(f"your age is {age}")
+
+
+# x = input("Type something = ")
+# print("you entered")
+# print(x)
+
+
+name = input("Entered your name = ")
+age = input("Entered your age = ")
+gender = input("Entered your gender = ")
+
+print(f"Enter your name {name}")
+print(f"Enter your age {age}")
+print(f"Enter your gender {gender}")
---
+++ Basics/tut9-add_number_input.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter first number = "))
+num2 = int(input("Enter second number = "))
+num3 = int(input("Enter third number = "))
+num4 = int(input("Enter fourth number = "))
+num5 = int(input("Enter fifth number = "))
+
+total = num1+num2+num3+num4+num5
+print(f"Your total is = {total}") 
+            
