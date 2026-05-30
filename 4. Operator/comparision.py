0ab1454e66c21c527bcc55bb58e9f7e5308861cd
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-05-30 20:03:59 +0530

    The new one

---
+++ 4. Operator/comparision.py
@@ -0,0 +1,26 @@
+# Comparision Operator 
+
+a = 25
+b = 15
+print(a < b)
+
+a = 34
+b = 76
+print(a > b)
+
+a = 789
+b = 655
+print(a >= b)
+
+a = 6456
+b = 8875
+print(a <= b)
+print(a != b)
+
+a = 64739
+b = 85628
+print(a != b)
+
+a = 354583
+b = 153895
+print(a == b)
