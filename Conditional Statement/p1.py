63cba1fdbc7f42462037c62e6facff226c07e709
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-05-31 18:56:59 +0530

    New file

---
+++ Conditional Statement/p1.py
@@ -0,0 +1,14 @@
+"""
+Ask 2 numbers from user 
+Print number which is the greatest
+"""
+
+num1 = int(input("Enter number 1 = "))
+num2 = int(input("Enter number 2 = "))
+
+if num1 > num2 :
+    print("Number 1 is greater")
+
+else :
+    print("Number 2 is greater")
+
