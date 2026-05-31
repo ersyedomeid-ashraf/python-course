44449552bb02ff3e821926b7495c998ac9b3ac1e
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-05-31 18:46:51 +0530

    New one

---
+++ Conditional Statement/if-else.py
@@ -0,0 +1,16 @@
+# Conditional Statement
+
+# Ask age from user 
+# you can vote if 18 above
+
+age = int(input("Enter your age = "))
+
+if age >= 18 :
+    print("You can vote")                         #In the beginning the space is called as indentation.
+    print("You are allowed to vote")              #Indentation refers to the space at the beginning of a code line.
+
+else :
+    print("You cannot vote")                      #In the beginning the space is called as indentation.
+    print("You are not allowed to vote")          #Indentation refers to the space at the beginning of a code line.
+
+   
