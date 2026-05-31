e46cf0c58c4c7e9841e4a00ec3ca1af183cb077a
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-05-31 19:06:23 +0530

    New one

---
+++ Conditional Statement/p2.py
@@ -0,0 +1,13 @@
+"""
+Ask number form the user 
+print if the number is odd or even 
+"""
+
+num = int(input("Enter number = "))
+
+if num % 2 == 0 :
+    print("Even")
+
+else :
+    print("Odd")
+    
