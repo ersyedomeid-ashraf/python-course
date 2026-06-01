43aca13a9d78475dddb94ae5be3f5534edb706d4
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-06-01 17:33:07 +0530

    New one

---
+++ Questions/11.py
@@ -0,0 +1,14 @@
+"""
+Write a number that takes two numbers as input and checks if the 
+first number is divisible by the second number.
+"""
+
+num1 = int(input("Enter a number 1 = "))
+num2 = int(input("Enter a number 2 = "))
+
+if num1%num2==0 :
+    print("yes, it's divisible by 2")
+
+else :
+    print("No, it's not divisible by 2")
+ 
