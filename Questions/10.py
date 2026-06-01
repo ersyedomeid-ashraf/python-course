15c9e96d1ddad8d82576e2ce8df0f0135e39ecab
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-06-01 17:09:26 +0530

    New one

---
+++ Questions/10.py
@@ -0,0 +1,13 @@
+"""
+Write a program that takes a character as input and prints whether 
+it's vowels or constatnt.
+"""
+
+char = input("Enter a character = ")
+
+if char=="a" or char=="e" or char=="i" or char=="o" or char=="u" :
+    print("Vowel")
+
+else :
+    print("Consonant")
+    
