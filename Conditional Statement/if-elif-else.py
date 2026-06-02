0dd42f6069baf6c78e319944f1ea855afbd1480c
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-06-02 18:19:56 +0530

    New one

---
+++ Conditional Statement/if-elif-else.py
@@ -0,0 +1,15 @@
+"""
+Ask two number from user, print which is greatest.
+"""
+
+num1 = int(input("Enter number 1 = "))
+num2 = int(input("Enter number 2 = "))
+
+if num1 > num2 :
+    print("Num1 is greatest")
+
+elif num2 > num1 :
+    print("Num2 is greatest")
+
+else :
+    print("Both are equal")
