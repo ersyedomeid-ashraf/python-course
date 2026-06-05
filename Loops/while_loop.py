c1e49f5a41be80d03f4bbe8c84d12354a5b4cdb4
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-06-05 17:33:46 +0530

    New one code

---
+++ Loops/while_loop.py
@@ -0,0 +1,19 @@
+"""
+Write a program to print "Hello World" 10 times.
+"""
+
+i = 1 
+while i <= 10 :
+    print("Hello World")
+    i = i+1 
+
+
+"""
+Write a program to print your name 15 times.
+"""
+
+i = 1 
+while i <= 10 :
+    print("Syed Omeid Ashraf")
+    i = i+1 
+    
