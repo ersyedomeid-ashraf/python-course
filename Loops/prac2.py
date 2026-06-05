090f2fe94d3fcb731de247475ff4f9277890a9ef
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-06-05 18:22:54 +0530

    New one code

---
+++ Loops/prac2.py
@@ -0,0 +1,11 @@
+"""
+Write a program to print 1 to 20 even numbers. 
+"""
+
+# loop 2 to 20,        i = i + 2
+
+i = 1 
+while i <= 50 :
+    if i%2 == 0 :
+         print(i, end = " ")
+    i = i + 1 
