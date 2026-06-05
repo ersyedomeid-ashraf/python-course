5e8c179e6b7189ca28f0d0abe3aff6c3a6e01cef
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-06-05 17:40:35 +0530

    New one code

---
+++ Loops/prac1.py
@@ -0,0 +1,18 @@
+"""
+Write a program to print 1 to 10 using while loop.
+"""
+
+i = 1 
+
+while i <= 10 :
+    print(i)
+    i = i + 1 
+
+
+# If we want in single line we'll write in this form. 
+
+i = 1
+
+while i <= 10 :
+    print(i, end = " ")
+    i = i + 1 
