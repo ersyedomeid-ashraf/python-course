b2e0755e8d17fa8ee9ba906e99afd225c57ac520
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-06-06 15:38:55 +0530

    New one

---
+++ Questions/21.py
@@ -0,0 +1,12 @@
+"""
+Write a program to ask a number (N) from user. Print all the numbers from N to 1. 
+"""
+
+
+num = int(input("Enter a number = "))
+
+i = num 
+while i >= 1 :
+    print(i, end = " ")
+
+    i = i - 1 
