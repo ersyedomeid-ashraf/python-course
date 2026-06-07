12240c4bd125adc2076b5a90fbdb19f1c2296ab8
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-06-07 23:43:03 +0530

    Solve 'for loop' problems

---
+++ Questions/31.py
@@ -0,0 +1,26 @@
+"""
+Write a program to calculate product all the number from 1 to 10.
+"""
+
+
+product = 1
+
+for i in range (1, 11) :
+    product = product * i
+
+
+print(f"Your answer is {product}")
+
+
+
+
+# Calculate product all the number from 1 to 18.
+
+
+product = 1 
+
+for i in range (1, 18) :
+    product = product * i 
+
+
+print(f"Your answer is {product}")
