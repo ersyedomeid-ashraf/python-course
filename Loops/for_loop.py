b7ca4f6f446e29436349645ad2ddf2ae7f68729a
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-06-07 20:06:59 +0530

    Solve 'for loop' question

--- Loops/for_loop.py
+++ Loops/for_loop.py
@@ -14,3 +14,13 @@ Write a program to print 1 to 20 ..... 1 3 5 7 9 ...... 19
 
 for i in range(1, 21, 2) :       # 2 is the step size; it increases by 2 each time 
     print(i, end = " ")
+
+
+
+
+"""
+Write a program to print 10 to 0 
+"""
+
+for i in range(10,0,-1) :
+    print(i, end = " ") 
