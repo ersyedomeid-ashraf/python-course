778d75539f7f9e796bf9ce959da8f238a61c843e
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-06-07 23:32:31 +0530

    Solve 'for loop' problems

---
+++ Questions/30.py
@@ -0,0 +1,26 @@
+"""
+Write a program to calculate the sum of all the number from 1 to 10. 
+"""
+
+
+total = 0
+
+for i in range (1, 11) :
+    total = total + i 
+
+
+print(f"Your answer is {total}")
+
+
+
+
+# Calculate the sum of all the number from 1 to 200.
+
+
+total = 0 
+
+for i in range (1, 200) :
+    total = total + i 
+
+
+print(f"Your answer is {total}")
