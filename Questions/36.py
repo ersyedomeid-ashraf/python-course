c28505b11df46697bfca96440e927db6dd939d19
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-06-09 19:03:29 +0530

    Add 'Star Pattern' program

---
+++ Questions/36.py
@@ -0,0 +1,14 @@
+"""
+Write a program to print 
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+"""
+
+for i in range (1,6) :            # Rows 
+    for j in range (1,6) :        # Stars in each row 
+        print("*", end = " ")
+
+    print()
+
