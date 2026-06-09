8f197814545381bea6f958acc0996ebdf54121ad
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-06-09 19:19:19 +0530

    Add 'Number Pattern' programs

---
+++ Questions/37.py
@@ -0,0 +1,32 @@
+"""
+Write a program to print the following number pattern
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+"""
+
+
+for i in range (1,6) :
+    for j in range (1,6) :
+        print(j, end = " ")
+
+    print()
+
+
+
+"""
+Write a program to print the following number pattern 
+5 4 3 2 1
+5 4 3 2 1
+5 4 3 2 1
+5 4 3 2 1
+5 4 3 2 1
+"""
+
+for i in range (1,5) :
+    for j in range (5,0,-1) :
+        print(j, end = " ")
+
+    print()
