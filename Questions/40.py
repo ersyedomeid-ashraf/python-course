3a2719d95d0e47090501559b442a0d2d49304ed3
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-06-10 15:43:07 +0530

    Solve 'Number Triangle Pattern' questions

---
+++ Questions/40.py
@@ -0,0 +1,40 @@
+"""
+Write a program to print the following pattern 
+
+1 
+2 2 
+3 3 3 
+4 4 4 4 
+5 5 5 5 5 
+
+"""
+
+for i in range (1,6) :
+    
+    for j in range (1, i +1) :
+        print (i, end = " ")
+
+    print()       
+
+
+"""
+1
+2 2 
+3 3 3 
+4 4 4 4 
+5 5 5 5 5 
+6 6 6 6 6 6 
+7 7 7 7 7 7 7 
+8 8 8 8 8 8 8 8 
+9 9 9 9 9 9 9 9 9 
+10 10 10 10 10 10 10 10 10 10 
+
+"""
+
+
+for i in range (1,11) :
+
+    for j in range (1, i + 1) :
+        print(i, end = " ")
+
+    print()
