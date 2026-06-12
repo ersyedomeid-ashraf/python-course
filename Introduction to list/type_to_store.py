2ce87187b3d1802e516a3900f4eecae13f71ed9e
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-06-12 16:40:57 +0530

    What are lists

---
+++ Introduction to list/type_to_store.py
@@ -0,0 +1,6 @@
+x = [1, 2, 3, 344.3, 54.324, "BMW M5 Competition", True, False]
+
+y = [21.322, 53344, 3.43, "Python", True, False]
+
+print(x, y)
+print(type(x), (y))
---
+++ Introduction to list/what_are_list.py
@@ -0,0 +1,18 @@
+"""
+lists ->
+To store multiple values in a single variable.
+"""
+
+Humaid = [67, 87, 97, 57, 99]
+
+print(Humaid)
+
+print(type(Humaid))
+
+
+Syed = [76, 78, 98, 96, 65]
+Zahra = [99, 87, 98, 89, 90]
+
+print(Syed)
+
+print(Zahra)
