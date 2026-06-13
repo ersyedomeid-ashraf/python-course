3428030c0b3e9434f9811eabc6ba9e6997c03866
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-06-13 19:59:48 +0530

    Practice of Program

---
+++ Introduction to list/p1.py
@@ -0,0 +1,21 @@
+"""
+Practice of program
+"""
+
+Humaid = [67, 87, 97, 57, 99]
+Ashraf = [89, 87, 65, 43, 34, 56]
+print(Humaid)
+print(Ashraf)
+print(type(Humaid))
+print(type(Ashraf))
+
+
+Syed = [76, 78, 98, 96, 65]
+Zahra = [99, 87, 98, 89, 90]
+
+print(Syed)
+
+print(Zahra)
+
+print(type(Syed))
+print(type(Zahra))
