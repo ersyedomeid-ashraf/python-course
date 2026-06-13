00e3bfab054f3f9428c9776b252799472f8a434b
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-06-13 20:06:21 +0530

    Practice of Program

--- Introduction to list/p1.py
+++ Introduction to list/p1.py
@@ -29,3 +29,24 @@ z = [234.43, 455, 543, "Bareilly", True, False]
 
 print(x, y, z)
 print(type(x), (y), (z))
+
+
+a = [21, 34, 56.6, 78.9, "Ashraf", "BMW", 23.54, 234.78, 45.543]
+
+b = [34, 432, 46.32, 56.787, "Syed", "M5 Competition", 765.8, 98.09]
+
+print(a[3])
+
+print(b[5])
+
+
+i = ["You", 34, 66, "Win"]
+
+j = [87, 65, "lose", 54]
+
+print((i[-2]), (j[3]))
+
+print(type(a[4]))
+print(type(b[3]))
+print(type(i[1]))
+print(type(j[2]))
