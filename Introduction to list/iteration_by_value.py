99f0ae7b1d6cdd409ea5cb383bd06589defe8ac2
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-06-14 21:32:27 +0530

    Add Iteration by value program

---
+++ Introduction to list/iteration_by_value.py
@@ -0,0 +1,8 @@
+a = [43, 65, 43, "Mahindra", "Scorpio", 45, 64, 65, 87, 99, 76]
+
+for i in a:
+    print(i)
+
+b = [43, 667, 876, 66, 555, "VW", "Virtus", 908, 879, 787, 883]
+for i in b:
+    print(i, end="")
