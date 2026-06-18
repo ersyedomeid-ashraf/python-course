ffdc83bd2f66a94b225579ebefbb1c5ac68ad65a
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-06-18 23:57:57 +0530

    Add program to merge two lists into a third list

---
+++ Questions/62.py
@@ -0,0 +1,19 @@
+"""
+Write a program to start by creating two seperate lists with random numbers. Then, create  third list
+that merges he numbers from he first second lists together.
+"""
+
+list1 = [1, 3, 5, 50, 68]
+list2 = [3, 5, 90, 65, 76]
+
+result = []
+
+for num in list1:
+    result.append(num)
+
+
+for num in list2:
+    result.append(num)
+
+
+print(result)
