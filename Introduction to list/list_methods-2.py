84d08992b9dad57aebe27773a8bb45266c47ef47
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-06-19 17:29:58 +0530

    Add python list methods

---
+++ Introduction to list/list_methods-2.py
@@ -0,0 +1,14 @@
+a = [32, 23, 45, "BMW", True, -65, 89, 98]
+
+pos = a.index(89)  # Get element position
+print(pos)
+
+
+a = [23, 2, 4, 3, 56, 76, 7, 6, 8, 90]
+a.sort()  # It arrange in ascending order
+print(a)
+
+
+a = [3, 7, 64, 4, 32, 56, 67, 89, 90]
+a.reverse()  # It reverse the list
+print(a)
