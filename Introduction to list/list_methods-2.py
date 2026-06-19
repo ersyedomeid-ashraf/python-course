abcc092acc336c9e874d1caf31e8e9fac36f2541
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-06-19 17:54:39 +0530

    Add python list methods

--- Introduction to list/list_methods-2.py
+++ Introduction to list/list_methods-2.py
@@ -12,3 +12,44 @@ print(a)
 a = [3, 7, 64, 4, 32, 56, 67, 89, 90]
 a.reverse()  # It reverse the list
 print(a)
+
+
+"""
+Write a program to print the list in descending order 
+"""
+
+a = [10, 43, 23, 45, 33, 22, 34, 87, 65, 90, 98, 45]
+
+a.sort()
+a.reverse()
+print(a)
+
+
+a = [33, 22, 43, 89, 70, 89, 98, 77, 43, 21, 25]
+
+a.sort(reverse=True)
+print(a)
+
+
+a = [23, 43, 54, 65, -98, -81, "Virtus", "Slavia", 34.5]
+
+a.append(["BMW", "Code", "Python", 200])  # Add single element at end of list
+print(a)
+
+
+a = [23, 43, 54, 65, -98, -81, "Virtus", "Slavia", 34.5]
+
+a.extend(["BMW", "Code", "Python", 200])  # Add multiple elements at end of list
+print(a)
+
+
+a = [23, 43, 54, 65, -98, -81, 43, "Slavia", 43, 34.5]
+
+r = a.count(43)
+print(r)
+
+
+a = [33, 22, 43, 89, 70, 89, 98, 77, 43, 21, 25]
+
+a.clear()
+print(a)
