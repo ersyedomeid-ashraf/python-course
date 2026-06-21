4689e4d312cd0bc751a377a66f39477dc7d0e5ee
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-06-21 17:00:28 +0530

    Add program to find average of numbers in a list

---
+++ Questions/66.py
@@ -0,0 +1,18 @@
+"""
+Write a program to find the average of all the numbers present in the list.
+"""
+
+my_list = [56, 68, 100, 5, 90, 556]
+
+average = sum(my_list) / len(my_list)
+
+print("Average =", average)
+
+
+# Another one
+
+my_list = [22, 32, 45, 67, 87, 89, 90, 23]
+
+average = sum(my_list) / len(my_list)
+
+print("Average =", average)
