f56ea70a7e42e563cf1e4dfdaf4df01c1d01bf55
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-06-21 17:48:38 +0530

    Added program to count occurrences of list elements and identify the highest frequency item

---
+++ Questions/67.py
@@ -0,0 +1,19 @@
+"""
+Write a python code to find the occurrence of each element in a list
+and print the element with the highest occurrence
+"""
+
+my_list = [2, 4, 6, "code", 4, "python", 2, "code", 4, 1, 3, "billa"]
+
+for num in my_list:
+    c = my_list.count(num)  # count occurrence of current element
+    print(f"{num} occurrs {c} times")
+
+
+# Another one
+
+my_list = [2, 4, 8, 5, "laila", 4, 3, 4, "code", "BMW", 6, 9]
+
+for num in my_list:
+    c = my_list.count(num)
+    print(f"{num} occurrs {c} times")
