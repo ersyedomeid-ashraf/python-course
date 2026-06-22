e3f41b68026843e20b2437a84e427a90d607fdb3
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-06-22 06:03:34 +0530

    Add program to calculate product of all list elements

---
+++ Questions/70.py
@@ -0,0 +1,25 @@
+"""
+Make a program that takes a a list of integers and returns the product
+of all the elements.
+"""
+
+my_list = [23, 54, 65, 45, 98, 77]
+
+product = 1
+
+for num in my_list:
+    product = product * num
+
+print(product)
+
+
+# Another one
+
+my_list = [23, 54, 65, 45, 98, 77, 84, 96, 29, 72, 90]
+
+product = 1
+
+for num in my_list:
+    product = product * num
+
+print(product)
