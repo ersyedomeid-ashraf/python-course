5e5a2d54f57aa98e59dbfe4776e22a2562e9ae38
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-06-22 06:17:43 +0530

    Add program to find prime numbers in a list

---
+++ Questions/71.py
@@ -0,0 +1,15 @@
+"""
+Write a program to find and print all prime numbers within a given list.
+"""
+
+my_list = [2, 3, 5, 7, 8, 9, 10, 12, 15, 17]
+
+for num in my_list:
+    factors = 0
+    for i in range(1, num + 1):
+
+        if num % i == 0:
+            factors += 1
+
+    if factors == 2:
+        print(num)
