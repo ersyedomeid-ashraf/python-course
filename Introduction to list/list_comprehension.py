51ac32a4887b2ece59308c38a62b0cde3c547ede
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-06-23 16:35:17 +0530

    Add list comprehension program for divisible numbers

--- Introduction to list/list_comprehension.py
+++ Introduction to list/list_comprehension.py
@@ -34,3 +34,16 @@ print(my_list)
 
 my_list = [i for i in range(1, 31) if i % 2 == 0]
 print(my_list)
+
+
+"""
+Write a Python program using list comprehension to create a list of numbers
+ between 3 and 100 that are divisible by both 2 and 3.
+"""
+
+
+start = int(input("Enter start number = "))
+end = int(input("Enter end number ="))
+
+my_list = [i for i in range(start, end + 1) if i % 2 == 0 and i % 3 == 0]
+print(my_list)
