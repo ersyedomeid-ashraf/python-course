f993ba9fedabc32dfcd721be188318d0e7bb52a1
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-06-26 18:33:18 +0530

    Add string operators program

---
+++ Strings/string_operators.py
@@ -0,0 +1,35 @@
+# ASCII Codes
+
+first_ch = "a"
+second_ch = "A"
+
+print(first_ch > second_ch)
+print(first_ch < second_ch)
+
+
+first_ch = "Mahesh"
+second_ch = "mahesh"
+
+print(first_ch > second_ch)
+print(first_ch < second_ch)
+
+
+first_ch = "mahesH"
+second_ch = "mahesh"
+
+print(first_ch > second_ch)
+print(first_ch < second_ch)
+
+
+ch = "a"
+print(ord(ch))
+
+
+ch = "U"
+print(ord(ch))
+
+num = 98
+print(chr(num))
+
+num = 75
+print(chr(num))
