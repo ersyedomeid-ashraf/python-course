e66c82a9ddee09ef17a296f4e5d72d72c7a3ab6f
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-06-26 18:47:19 +0530

    Add string slicing programs

---
+++ Strings/string_slicing.py
@@ -0,0 +1,23 @@
+a = "python and code"
+b = a[0:7]  # Get the first 7 characters from the string.
+print(b)
+
+
+a = "python and code"
+b = a[0:]  # Get the complete string.
+print(b)
+
+
+a = "python and code"
+b = a[7:]  # Get the string from index 7 to the end.
+print(b)
+
+
+a = "python and code"
+b = a[-4:]  # Get the last four character of the string
+print(b)
+
+
+a = "python and code"
+b = a[::-1]  # Reverse the string
+print(b)
