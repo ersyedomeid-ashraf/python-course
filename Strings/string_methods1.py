b8c3bae32008c070af4a9e43933061eafa0cc462
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-06-27 13:24:21 +0530

    Add string methods program

---
+++ Strings/string_methods1.py
@@ -0,0 +1,68 @@
+"""
+Title, Capitalized, upper, lower, swapcase,
+isupper, islower, isalpha, isalnum, isspace
+"""
+
+a = "syed humaid ashraf"
+x = a.title()
+print(x)
+
+
+a = "syed humaid ashraf"
+x = a.capitalize()
+print(x)
+
+
+a = "syed humaid ashraf"
+x = a.upper()
+print(x)
+
+
+a = "syed humaid ashraf"
+x = a.lower()
+print(x)
+
+
+a = "syed humaid ashraf"
+x = a.swapcase()
+print(x)
+
+
+a = "syed humaid ashraf"
+x = a.isupper()
+print(x)
+
+
+a = "syed humaid ashraf"
+x = a.islower()
+print(x)
+
+
+a = "BareillyUttarPradesh"
+x = a.isalpha()
+print(x)
+
+
+a = "Syed1humaid2ashraf"
+x = a.isalnum()
+print(x)
+
+
+a = "223454398700"
+x = a.isdigit()
+print(x)
+
+
+a = "syed humaid ashraf"
+x = a.isspace()
+print(x)
+
+
+a = "surya1234"
+
+if a.isdigit():
+    a = int(a)
+    print(a, type(a))
+
+else:
+    print("Cannot be converted into integer")
