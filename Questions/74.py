a09315bbb063bf747890b26d6b301fab30b44f3e
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-06-27 15:05:28 +0530

    Add alphabet counting program using isalpha() and ASCII

---
+++ Questions/74.py
@@ -0,0 +1,29 @@
+"""
+Write a program to ask a string from user. Count how many alphabets are
+there in that string.
+"""
+
+my_string = "abcd1234"
+
+count = 0
+
+for ch in my_string:
+    if ch.isalpha():
+        count = count + 1
+
+print(count)
+
+
+# We Write this code in the ASCII form
+
+my_string = "abcd1234"
+
+count = 0
+
+for ch in my_string:
+    ascii = ord(ch)
+
+    if (ascii >= 65 and ascii <= 90) or (ascii >= 97 and ascii <= 122):
+        count += 1
+
+print(count)
