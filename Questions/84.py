f4e6803f032d3d062620262a4ce77951effc6f56
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-07-02 23:50:36 +0530

    Count character frequency using dictionary

---
+++ Questions/84.py
@@ -0,0 +1,18 @@
+"""
+Ask a string from user. Display the dictionary where each key is a character and value
+is the frequency of that character that comes in that string.
+"""
+
+my_string = input("Enter a string = ")
+
+freq = {}
+
+for ch in my_string:
+    if ch in freq:
+        freq[ch] += 1
+
+    else:
+        freq[ch] = 1
+
+
+print(freq)
