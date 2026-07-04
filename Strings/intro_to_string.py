f271747091ed678fa42d7e1ce2725514b1516acf
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-07-04 19:12:31 +0530

    Practice string program

--- Strings/intro_to_string.py
+++ Strings/intro_to_string.py
@@ -64,3 +64,33 @@ for ch in s:
     count += 1
 
 print(count)
+
+
+"""
+String - String is a sequence of character enclosed within single quotes (' ') or double quotes (" ").
+A string cannot be changed after it is created.
+"""
+
+a = "Python Code"
+
+print(a, type(a))
+
+print(a[2])
+print(a[3])
+print(a[-4])
+
+# Iterate string
+
+my_string = "Python Code"
+# By index, By value
+
+for index in range(0, len(my_string)):  # by index
+    print(my_string[index])
+
+
+for ch in my_string:  # by value
+    print(ch)
+
+
+for i in range(len(my_string) - 1, -1, -1):
+    print(my_string[i])
