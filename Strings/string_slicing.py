a3ced32bb09a8da6e886b76d79c63d625443b128
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-07-04 19:11:00 +0530

    Practice string slicing program

--- Strings/string_slicing.py
+++ Strings/string_slicing.py
@@ -21,3 +21,13 @@ print(b)
 a = "python and code"
 b = a[::-1]  # Reverse the string
 print(b)
+
+
+a = "code and debug python"
+b = a[7:]  # Get the string from index 7 to the end.
+print(b)
+
+
+a = "remember it"
+b = a[-4:]  # Get the last four character of the string
+print(b)
