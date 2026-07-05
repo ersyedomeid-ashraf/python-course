0c1d3e7bfe5dcb38cf555f7a59940b1786e2c4fa
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-07-05 20:00:38 +0530

    practice logical program

--- 4. Operator/logical.py
+++ 4. Operator/logical.py
@@ -1,9 +1,10 @@
-# Logical Operator 
+# Logical Operator
 # Result is always in boolean form (Yes/No) (True/False)
 
 """
-Not reverse the result 
+Not reverse the result
 """
+
 physics = 86
 chemistry = 15
 
@@ -11,8 +12,7 @@ print(physics > 33 and chemistry > 33)
 print(physics > 33 or chemistry > 33)
 
 print(physics > 33)
-print(not physics > 33) 
-
+print(not physics > 33)
 
 
 english = 33
@@ -22,3 +22,14 @@ science = 98
 print(english > 33 and mathematic > 33 and science > 33)
 print(english > 33 or mathematic > 33 or science > 33)
 print(not english > 33, not mathematic > 33, not science > 33)
+
+
+english = 33
+mathematic = 78
+science = 98
+hindi = 76
+
+print(english > 33 and mathematic > 33 and science > 33 or hindi > 33)
+print(english > 33 or mathematic > 33 or science > 33 or hindi > 33)
+print(english > 33 or mathematic > 33 or science > 33 or hindi > 33)
+print(not english > 33, not mathematic > 33, not science > 33, not hindi > 33)
