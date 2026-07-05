5d5818fd6bf6143faf26a0fbfa09fa026767c7ad
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-07-05 19:57:12 +0530

    Practice if-elif-else program

--- Conditional Statement/if-elif-else.py
+++ Conditional Statement/if-elif-else.py
@@ -5,11 +5,28 @@ Ask two number from user, print which is greatest.
 num1 = int(input("Enter number 1 = "))
 num2 = int(input("Enter number 2 = "))
 
-if num1 > num2 :
+if num1 > num2:
     print("Num1 is greatest")
 
-elif num2 > num1 :
+elif num2 > num1:
     print("Num2 is greatest")
 
-else :
+else:
+    print("Both are equal")
+
+
+"""
+Ask five number from user, print which is greatest.
+"""
+
+num1 = int(input("Enter number 1 = "))
+num2 = int(input("Enter number 2 = "))
+
+if num1 > num2:
+    print("Num1 is greatest")
+
+elif num2 > num1:
+    print("Num2 is greatest")
+
+else:
     print("Both are equal")
