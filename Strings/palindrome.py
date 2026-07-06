090435583897353775a056111160fb7a41456f83
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-07-06 19:34:08 +0530

    Practice palindrome program

--- Strings/palindrome.py
+++ Strings/palindrome.py
@@ -9,3 +9,12 @@ if my_string == my_string[::-1]:
 
 else:
     print("It is not a palindrome")
+
+
+my_string = input("Enter a string = ")
+
+if my_string == my_string[::-1]:
+    print("It is a palindrome")
+
+else:
+    print("It is not a palindrome")
