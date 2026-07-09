853b425d3ea45b60a604d8f1d0e31ec94c010fdd
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-07-09 18:00:48 +0530

    Add program to remove all elements from a given sets

---
+++ Questions/92.py
@@ -0,0 +1,27 @@
+"""
+Write a program to remove all elements from a given sets.
+"""
+
+my_set = {34, 54, 46, 78, 98}
+
+my_set.clear()
+print(my_set)
+
+
+my_set = {34, 32, 56, 78, 98, 766, 778, 90}
+
+my_set.clear()
+print(my_set)
+
+
+my_set = {33, 45, 90, 98, 77, 67, 68, 78, 98, 56, 4556, 778887, 544, 5678, 889}
+
+my_set.clear()
+print(my_set)
+
+
+my_set = {34, 34, 32, 34, 332, 44, 32, 44}
+
+
+my_set.clear()
+print(my_set)
