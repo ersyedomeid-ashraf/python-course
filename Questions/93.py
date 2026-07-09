d4625e4bfbb2b394615e773a4bafb760e32b2606
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-07-09 21:57:33 +0530

    Implement program to find the length of a set

---
+++ Questions/93.py
@@ -0,0 +1,15 @@
+"""
+Write a program to find the length of a set.
+"""
+
+my_set = {45, 87, 77, 90, "Majnu", 34}
+
+count = 0
+for i in my_set:
+    count += 1
+
+print(count)
+
+# in short way we'll write
+
+print(len(my_set))
