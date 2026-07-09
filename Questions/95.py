7e3920e8951f89fb34935ccf4485a8ab9a7c3f1c
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-07-09 22:16:09 +0530

    Add program to remove duplicate characters from a string

---
+++ Questions/95.py
@@ -0,0 +1,23 @@
+"""
+Write a program to ask a string from a user, remove all the duplicates from that string
+and print hat string again (order doesn't matter).
+"""
+
+my_string = "aaaaaaeeeerrrrooooppppllllaaaannnneeee"
+
+result = set(my_string)
+print(result)
+joined_string = " ".join(result)
+print(joined_string)
+
+
+# Another one
+
+my_string = "ttttttttteeeeeeerrrrrrriiiiiiiiiimmmmmmeeeeerrrrrriiiiiiii"
+
+
+result = set(my_string)
+print(result)
+
+joined_string = " ".join(result)
+print(joined_string)
