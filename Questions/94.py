751a88e585946ff09d448e06fc6bd28120beb208
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-07-09 22:07:53 +0530

    Implement program two given sets have no elements in common

---
+++ Questions/94.py
@@ -0,0 +1,30 @@
+"""
+Write a program to check if two given sets have no elements in common.
+"""
+
+set1 = {45, 67, 45, "Aaruhiii", 68, 90, 87}
+set2 = {76, 57, 45, 67, "Aaruhiii", 90, -65, 1}
+
+result = set1.intersection(set2)
+
+if len(result) == 0:
+    print("Both sets have nothing in common")
+
+else:
+    print("Sets have something in common")
+    print(result)
+
+
+# Another one
+
+set1 = {34, 45, 67, 87, "Naina", 90, -98}
+set2 = {54, 56, 76, 78, "Sunaina", 89, -43, 2}
+
+result = set1.intersection(set2)
+
+if len(result) == 0:
+    print("Both sets have nothing in common")
+
+else:
+    print("Sets have something in common")
+    print(result)
