fa72ea0b3302b97531eca665a179fd84bfbce9cd
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-07-10 15:35:05 +0530

    Practice remove all the duplicates from that string

--- Questions/95.py
+++ Questions/95.py
@@ -21,3 +21,16 @@ print(result)
 
 joined_string = " ".join(result)
 print(joined_string)
+
+
+"""
+Write a program to ask a string from a user, remove all the duplicates from that string
+and print hat string again (order doesn't matter).
+"""
+
+my_string = "MMMMMMMMiiiiiiiiiiiyyyyyyyaaaaaaaaHHHHHHHuuuuuzzzzzzzuuuuurrrrrrrrr"
+
+result = set(my_string)
+print(result)
+joined_string = " ".join(result)
+print(joined_string)
