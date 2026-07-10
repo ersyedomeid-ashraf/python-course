5f8b3b15e0af6bf3805f224856bfe2245b9bf72a
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-07-10 15:32:29 +0530

    Practice check if two given sets have no elements in common

--- Questions/94.py
+++ Questions/94.py
@@ -28,3 +28,20 @@ if len(result) == 0:
 else:
     print("Sets have something in common")
     print(result)
+
+
+"""
+Write a program to check if two given sets have no elements in common.
+"""
+
+set1 = {45, 67, 45, "Aaruhiii", 68, 90, "Kallan", 87}
+set2 = {76, "Kallan", 57, 45, 67, "Aaruhiii", 90, -65, 1}
+
+result = set1.intersection(set2)
+
+if len(result) == 0:
+    print("Both sets have nothing in common")
+
+else:
+    print("Sets have something in common")
+    print(result)
