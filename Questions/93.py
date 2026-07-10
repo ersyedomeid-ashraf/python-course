9da9e5cdef2755c7dacc7fb1230d30d256e6a786
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-07-10 15:29:57 +0530

    Practice find the length of a set

--- Questions/93.py
+++ Questions/93.py
@@ -13,3 +13,16 @@ print(count)
 # in short way we'll write
 
 print(len(my_set))
+
+
+"""
+Write a program to find the length of a set.
+"""
+
+my_set = {45, 87, "Kaaluu", 77, 90, "Majnu", 34}
+
+count = 0
+for i in my_set:
+    count += 1
+
+print(count)
