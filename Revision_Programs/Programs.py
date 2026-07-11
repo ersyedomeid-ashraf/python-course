79c5fe19ee44a8aa1a9ea0f32171520dcf541463
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-07-11 08:23:23 +0530

    Implement program print all no. from 1 to that number

---
+++ Revision_Programs/Programs.py
@@ -0,0 +1,23 @@
+"""
+Write a program to ask a number from user. Print all the number from 1 to that number.
+"""
+
+num = int(input("Enter a number = "))
+
+i = 1
+while i <= num:
+    print(i)
+
+    i = i + 1
+
+
+# if we take in single line
+
+
+num = int(input("Enter a number = "))
+
+i = 1
+while i <= num:
+    print(i, end=" ")
+
+    i = i + 1
