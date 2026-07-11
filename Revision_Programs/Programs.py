d4aef19fdd5689b4a7f31cb0252e3a7d85b74951
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-07-11 08:30:03 +0530

    Implement program to print star pattern triangle

--- Revision_Programs/Programs.py
+++ Revision_Programs/Programs.py
@@ -72,3 +72,24 @@ for num in numbers:
 
 print("Original list:", numbers)
 print("Updated list:", new_list)
+
+
+"""
+Write a program to print the following pattern 
+       
+         * 
+      * * *
+    * * * * *
+  * * * * * * *
+* * * * * * * * * 
+
+"""
+
+for i in range(1, 6):
+    for j in range(i, 5):
+        print(" ", end=" ")
+
+    for k in range(1, (i * 2)):
+        print("*", end=" ")
+
+    print()
