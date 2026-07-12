86f05c88781e16e4d5568f6e286fa3f8035f0955
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-07-12 18:14:52 +0530

    Implement program to check whether a givenm number is positive,negative or equal to zero

--- Revision_Programs/Programs.py
+++ Revision_Programs/Programs.py
@@ -177,3 +177,33 @@ elif num2 > num1:
 
 else:
     print("Both are equal")
+
+
+"""
+Write a program to check whether a given number is positive, negative or equal to zero. 
+"""
+# This is method 1 :
+num = int(input("Enter a number = "))
+
+if num >= 0:
+    if num > 0:
+        print("Postive")
+
+    else:
+        print("Equal to zero ")
+
+else:
+    print("Negative")
+
+
+# This is method 2 :
+
+if num > 0:
+    print("Positive")
+
+else:  # <=0
+    if num == 0:
+        print("Equal to zero")
+
+    else:
+        print("Negative")
