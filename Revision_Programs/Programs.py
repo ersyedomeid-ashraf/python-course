b293ebac39a78750e795c9cde612292cfe864d0d
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-07-12 18:11:50 +0530

    Add program to ask two number from user print which is greatest

--- Revision_Programs/Programs.py
+++ Revision_Programs/Programs.py
@@ -160,3 +160,20 @@ print(type(a[4]))
 print(type(b[3]))
 print(type(i[1]))
 print(type(j[2]))
+
+
+"""
+Ask two number from user, print which is greatest.
+"""
+
+num1 = int(input("Enter number 1 = "))
+num2 = int(input("Enter number 2 = "))
+
+if num1 > num2:
+    print("Num1 is greatest")
+
+elif num2 > num1:
+    print("Num2 is greatest")
+
+else:
+    print("Both are equal")
