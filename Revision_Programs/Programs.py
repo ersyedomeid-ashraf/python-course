9c1b13bd029f69e2f0fc31b737d4bbb026d8fa25
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-07-14 21:14:16 +0530

    Add list common example program

--- Revision_Programs/Programs.py
+++ Revision_Programs/Programs.py
@@ -377,3 +377,15 @@ if num in my_set:
 
 else:
     print("No")
+
+
+# list common example
+
+a = [1, 2, 3, 4, 5]
+b = [4, 5, 6, 7, 8, 9]
+
+c = set(a)
+d = set(b)
+
+result = list(c.intersection(d))
+print(result)
