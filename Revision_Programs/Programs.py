398b97b077d9030383a210107041ab5d6b757445
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-07-14 21:17:17 +0530

    Implement calculate how many no. are divisible by both 15 and 16 betwen 1 to 2000

--- Revision_Programs/Programs.py
+++ Revision_Programs/Programs.py
@@ -389,3 +389,17 @@ d = set(b)
 
 result = list(c.intersection(d))
 print(result)
+
+
+# Calculate how many numbers are divisible by both 15 and 16 between 1 to 2000.
+
+
+count = 0
+
+for i in range(1, 2001):
+
+    if i % 15 == 0 and i % 16 == 0:
+        count = count + 1
+
+
+print(count)
