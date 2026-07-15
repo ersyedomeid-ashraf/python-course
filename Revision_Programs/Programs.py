63e678dba104eb67be92f07c0733e7f91cbced9b
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-07-15 19:09:01 +0530

    Implement program to convert kilometer to miles

--- Revision_Programs/Programs.py
+++ Revision_Programs/Programs.py
@@ -457,3 +457,17 @@ print(f"Swapped values: a={a}, b = {b}")
 import random
 
 print(f"Random number: {random.randint(1, 100)}")
+
+
+# Write a program to convert kilometer to miles.
+
+kilometers = float(input("Enter distance in kilometer = "))
+
+
+# Conversion factor : 1 kilometer = 0.621371 miles
+
+conversion_factor = 0.621371
+
+miles = kilometers * conversion_factor
+
+print(f"{kilometers} kilometers is equal to {miles} miles")
