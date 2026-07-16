e42bfaac26135ed7558d93feb4bcddd46e00c4bb
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-07-16 17:15:00 +0530

    Implement program to count how many alphabets are there in that string

--- Revision_Programs/Programs.py
+++ Revision_Programs/Programs.py
@@ -641,3 +641,34 @@ words = words[::-1]
 
 result = " ".join(i for i in words)
 print(result)
+
+
+"""
+Write a program to ask a string from user. Count how many alphabets are
+there in that string.
+"""
+
+my_string = "abcd1234"
+
+count = 0
+
+for ch in my_string:
+    if ch.isalpha():
+        count = count + 1
+
+print(count)
+
+
+# Count alphabets using ASCII values.
+
+my_string = "abcd1234"
+
+count = 0
+
+for ch in my_string:
+    ascii = ord(ch)
+
+    if (ascii >= 65 and ascii <= 90) or (ascii >= 97 and ascii <= 122):
+        count += 1
+
+print(count)
