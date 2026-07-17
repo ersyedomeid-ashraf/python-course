e554bbc437e758ce2880904d46c8175b64c0aa25
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-07-17 10:03:17 +0530

    Implement basic function program

---
+++ Functions/intro.py
@@ -0,0 +1,22 @@
+"""
+Functions :- A function in Python is a block of code that performs a specific task. It is written
+once and can be used many times whenever needed.
+"""
+
+
+def greet():
+
+    print("Hello World")
+    print("How are you")
+
+
+greet()
+
+
+def greet():
+
+    print("I am fine")
+    print("My fav car is BMW M5 COMPETITION")
+
+
+greet()
