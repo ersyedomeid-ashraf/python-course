42d924284f6e513bd4a033aabee5bb7ebae69f74
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-07-17 18:43:15 +0530

    Implement add function program

---
+++ Functions/func.py
@@ -0,0 +1,31 @@
+def add():
+
+    num1 = 34
+    num2 = 55
+
+    print(num1 + num2)
+
+
+add()
+
+
+def add():
+
+    num1 = 34
+    num2 = 55
+
+    print(f"Sum = {num1 + num2}")
+
+
+add()
+
+
+def add():
+
+    num1 = int(input("Enter a num1 = "))
+    num2 = int(input("Enter a num2 = "))
+
+    print(f"Sum {num1 + num2}")
+
+
+add()
