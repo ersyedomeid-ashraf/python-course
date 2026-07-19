8cdbbb3a89bc85d369835d54f89189a95ce93b4b
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-07-19 16:19:41 +0530

    Implement named parameters in function

---
+++ Functions/named-parameters.py
@@ -0,0 +1,31 @@
+def total_marks(physics, chemistry, biology, maths, english):
+
+    print(f"Your marks in physics = {physics}")
+    print(f"Your marks in chemistry = {chemistry}")
+    print(f"Your marks in biology = {biology}")
+    print(f"Your marks in maths = {maths}")
+    print(f"Your marks in english = {english}")
+
+    total = physics + chemistry + biology + maths + english
+    print(f"Your total marks = {total}")
+
+
+total_marks(64, 78, 86, 98, 90)
+
+
+# Now we used Named Parameter
+
+
+def total_marks(physics, chemistry, biology, maths, english):
+
+    print(f"Your marks in physics = {physics}")
+    print(f"Your marks in chemistry = {chemistry}")
+    print(f"Your marks in biology = {biology}")
+    print(f"Your marks in maths = {maths}")
+    print(f"Your marks in english = {english}")
+
+    total = physics + chemistry + biology + maths + english
+    print(f"Your total marks = {total}")
+
+
+total_marks(chemistry=88, maths=90, english=99, biology=96, physics=80)
