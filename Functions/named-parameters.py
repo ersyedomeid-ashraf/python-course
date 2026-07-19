eaa89c04c6efc0349268bf0113fda9635eb6c884
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-07-19 16:25:38 +0530

    Implement positional and keyword arguments

--- Functions/named-parameters.py
+++ Functions/named-parameters.py
@@ -29,3 +29,22 @@ def total_marks(physics, chemistry, biology, maths, english):
 
 
 total_marks(chemistry=88, maths=90, english=99, biology=96, physics=80)
+
+
+# The first two values are given as positional arguments, and the remaining
+# values are given as named (keyword) arguments.
+
+
+def total_marks(physics, chemistry, biology, maths, english):
+
+    print(f"Your marks in physics = {physics}")
+    print(f"Your marks in chemistry = {chemistry}")
+    print(f"Your marks in biology = {biology}")
+    print(f"Your marks in maths = {maths}")
+    print(f"Your marks in english = {english}")
+
+    total = physics + chemistry + biology + maths + english
+    print(f"Your total marks = {total}")
+
+
+total_marks(78, 98, maths=90, english=99, biology=96)
