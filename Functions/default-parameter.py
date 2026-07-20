1bf6b84ca44f37e7b485b3457de65f479f96cdb2
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-07-20 22:39:13 +0530

    Implement default parameter in function

--- Functions/default-parameter.py
+++ Functions/default-parameter.py
@@ -47,3 +47,18 @@ def total_marks(physics, chemistry, biology=0, maths=0, english=0):
 
 
 total_marks(99, 98)
+
+
+def total_marks(physics, chemistry, biology, maths=0, english=0):
+
+    print(f"Your marks in physics = {physics}")
+    print(f"Your marks in chemistry = {chemistry}")
+    print(f"Your marks in biology = {biology}")
+    print(f"Your marks in maths = {maths}")
+    print(f"Your marks in english = {english}")
+
+    total = physics + chemistry + biology + maths + english
+    print(f"Your total marks = {total}")
+
+
+total_marks(99, 98, 99)
