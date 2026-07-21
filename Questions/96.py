3482e5394eb837a738560cd9213788fd56845793
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-07-21 14:23:30 +0530

    Add program to print the multiplication table for that number upto 10

---
+++ Questions/96.py
@@ -0,0 +1,36 @@
+"""
+Write a program to write a function that accepts an integer and prints the
+multiplication table for that number up to 10.
+"""
+
+
+def multiplication_table(num):
+    for i in range(1, 11):
+
+        print(f"{num} x {i} ={num*i}")
+
+
+multiplication_table(6)
+
+
+# Another one
+
+
+def multiplication_table(num):
+    for i in range(1, 11):
+
+        print(f"{num} x {i} ={num*i}")
+
+
+# Another one
+
+multiplication_table(86)
+
+
+def multiplication_table(num):
+    for i in range(1, 11):
+
+        print(f"{num} x {i} ={num*i}")
+
+
+multiplication_table(98)
