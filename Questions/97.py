67b3388039c0dc348873992c36dc94ebff9cced8
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-07-21 14:29:45 +0530

    Implement write a function that accepts an integer and prints whether it is odd or even

---
+++ Questions/97.py
@@ -0,0 +1,60 @@
+"""
+Write a program to write a function that accepts an integer and prints whether it is odd or even
+"""
+
+
+def even_odd(num):
+    if num % 2 == 0:
+        print("It is a even number")
+
+    else:
+        print("It is a odd number")
+
+
+even_odd(10)
+even_odd(9)
+
+
+# Another one
+
+
+def even_odd(num):
+    if num % 2 == 0:
+        print("It is a even number")
+
+    else:
+        print("It is a odd number")
+
+
+even_odd(64)
+even_odd(99)
+
+
+# Another one
+
+
+def even_odd(num):
+    if num % 2 == 0:
+        print("It is a even number")
+
+    else:
+        print("It is a odd number")
+
+
+even_odd(10099)
+even_odd(98874)
+
+
+# Another one
+
+
+def even_odd(num):
+    if num % 2 == 0:
+        print("It is a even number")
+
+    else:
+        print("It is a odd number")
+
+
+even_odd(154780)
+even_odd(935678)
