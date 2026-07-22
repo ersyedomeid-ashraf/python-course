b16d68475786a07ba069366fade7710160899c4f
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-07-22 20:14:40 +0530

    Implement function to find the largest of three numbers

--- Questions/98.py
+++ Questions/98.py
@@ -20,3 +20,24 @@ def largest(num1, num2, num3):
 
 
 largest(233, 465, 421)
+
+
+# Another one
+
+
+def largest(num1, num2, num3):
+
+    if num1 > num2 and num1 > num3:
+        print("num1")
+        print(f"{num1} is the largest")
+
+    elif num2 > num1 and num2 > num3:
+        print(num2)
+        print(f"{num2} is the largest")
+
+    else:
+        print("num3")
+        print(f"{num3} is the largest")
+
+
+largest(9898, 8989, 9988)
