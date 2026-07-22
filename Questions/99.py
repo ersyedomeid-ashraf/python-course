6809ba59789d2c3cc1d865e649f41c6a543aef87
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-07-22 20:36:53 +0530

    Implement function to print whether it is a prime number

--- Questions/99.py
+++ Questions/99.py
@@ -22,3 +22,31 @@ def is_prime(num):  # 30
 
 is_prime(10)
 is_prime(11)
+
+
+# Another one
+
+
+def is_prime(num):  # 99
+
+    factors = 0
+
+    for i in range(1, num + 1):  # 30 31 32.......98 99
+
+        if num % i == 0:
+            factors += 1
+
+    if factors == 2:
+        print("It is a prime number")
+
+    else:
+        print("It is not a prime number")
+
+
+is_prime(76)
+is_prime(13)
+is_prime(86)
+is_prime(12)
+is_prime(78)
+is_prime(24)
+is_prime(18)
