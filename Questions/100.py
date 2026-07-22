87f927ee3c0403a15e7f5ae69a1bf41794628349
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-07-22 20:46:48 +0530

    Implement function to print the sum and average of these numbers

--- Questions/100.py
+++ Questions/100.py
@@ -19,3 +19,23 @@ def sum_avg_lst(lst):
 
 sum_avg_lst([1, 2, 3, 4, 5])
 sum_avg_lst([59, -86, 74, 102, 97])
+
+
+# Another one
+
+
+def sum_avg_lst(lst):
+
+    total = 0
+
+    for num in lst:
+        total = total + num
+
+    print(f"Total of all numbers = {total}")
+
+    avg = total / len(lst)
+    print(f"Average of all numbers = {avg}")
+
+
+sum_avg_lst([8, 7, 6, 5, 44, 56, 78, 90])
+sum_avg_lst([98, -54, 898, 900, 787, 546, -8677, 988])
