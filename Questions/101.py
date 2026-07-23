ff60f0aa1866ff1a34f7f55f74e8b009c4343486
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-07-23 15:21:17 +0530

    Implement character frequency counter function

---
+++ Questions/101.py
@@ -0,0 +1,32 @@
+"""
+Write a program to write a function that accepts a string and prints the
+frequency of each character in the string.
+
+Example - HELLO
+
+{"H":1, "E":1, "L":2, "O":1}
+
+h occurs 1 times
+e occurs 1 times
+l occurs 2 times
+o occurs 1 times
+"""
+
+
+def charCount(string):
+
+    my_dict = dict()
+
+    for ch in string:
+
+        if ch not in my_dict:
+            my_dict[ch] = 1
+
+        else:
+            my_dict[ch] += 1
+
+    for k, v in my_dict.items():
+        print(f"{k} occurs {v} times")
+
+
+charCount("HEELLLLLOOOOOOOOOOO")
