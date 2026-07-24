67c1eb900f31743b4ba65ed60f1558efda103188
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-07-24 08:19:11 +0530

    Practice program to print whether it is a prime number

--- Revision_Programs/Programs.py
+++ Revision_Programs/Programs.py
@@ -760,3 +760,29 @@ def largest(num1, num2, num3):
 
 
 largest(9845674, 859057, 5875894)
+
+
+"""
+Write a program to write a function that takes an integer and prints whether
+it is a prime number.
+"""
+
+
+def is_prime(num):  # 30
+
+    factors = 0
+
+    for i in range(1, num + 1):  # 1 2 3 4 5 6......28 29 30
+
+        if num % i == 0:
+            factors += 1
+
+    if factors == 2:
+        print("It is a prime number")
+
+    else:
+        print("It is not a prime number")
+
+
+is_prime(10)
+is_prime(11)
