4bf72601730ab5229f550fbf47928833a4c63e7e
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-07-24 08:11:25 +0530

    Practice program to print whether it is odd or even

--- Revision_Programs/Programs.py
+++ Revision_Programs/Programs.py
@@ -704,3 +704,35 @@ def multiplication_table(num):
 
 
 multiplication_table(87)
+
+
+"""
+Write a program to write a function that accepts an integer and prints whether it is odd or even
+"""
+
+
+def even_odd(num):
+    if num % 2 == 0:
+        print("It is a even number")
+
+    else:
+        print("It is a odd number")
+
+
+even_odd(10)
+even_odd(9)
+
+
+# Another one
+
+
+def even_odd(num):
+    if num % 2 == 0:
+        print("It is a even number")
+
+    else:
+        print("It is a odd number")
+
+
+even_odd(64)
+even_odd(99)
