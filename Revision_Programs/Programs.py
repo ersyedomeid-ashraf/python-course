adb4dcfa74ecf8065f57498081cf6affb504afd9
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-07-24 08:15:54 +0530

    Practice program to print the largest among them in function

--- Revision_Programs/Programs.py
+++ Revision_Programs/Programs.py
@@ -736,3 +736,27 @@ def even_odd(num):
 
 even_odd(64)
 even_odd(99)
+
+
+"""
+Write a program to write a function that takes three number as a parameter
+and prints the largest among them.
+"""
+
+
+def largest(num1, num2, num3):
+
+    if num1 > num2 and num1 > num3:
+        print("num1")
+        print(f"{num1} is the largest")
+
+    elif num2 > num1 and num2 > num3:
+        print(num2)
+        print(f"{num2} is the largest")
+
+    else:
+        print("num3")
+        print(f"{num3} is the largest")
+
+
+largest(9845674, 859057, 5875894)
