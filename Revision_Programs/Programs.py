da71b702bb1211faa8d4756a7dfd647ad8da52b1
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-07-25 21:28:37 +0530

    Practice program to print whether it is a palindrome

--- Revision_Programs/Programs.py
+++ Revision_Programs/Programs.py
@@ -863,3 +863,23 @@ def charCount(string):
 
 
 charCount("HEELLLLLOOOOOOOOOOO")
+
+
+"""
+Write a program to write a function that takes a string and prints whether
+it is a palindrome.
+"""
+
+
+def check_palindrome(string):
+
+    if string == string[::-1]:
+        print("It is a palindrome")
+
+    else:
+        print("It is not a palindrome")
+
+
+check_palindrome("mom")
+check_palindrome("anaa")
+check_palindrome("madam")
