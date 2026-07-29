a1c026e488f70b735c17adf7608ba7d7875da253
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-07-29 15:29:11 +0530

    Practice program to remove all the numbers that can divisible by the number the user entered

--- Revision_Programs/Programs.py
+++ Revision_Programs/Programs.py
@@ -1298,3 +1298,22 @@ my_list = [21, 43, 45, 32, "Hurray", 89, 76, "Done", "Done"]
 for i in range(0, len(my_list)):
     if i % 2 == 0:
         print(my_list[i], end=" ")
+
+
+"""
+Write a program to ask the user for a number. Then, from a list of numbers, remove all the numbers
+that can divisible by the number the user entered.
+"""
+
+numbers = [10, 15, 20, 25, 30, 35, 40, 45, 50]  # List of numbers
+
+n = int(input("Enter a number: "))  # Ask the user for a number
+
+new_list = []  # Create a new list
+
+for num in numbers:  # Add only numbers that are not divisible by n
+    if num % n != 0:
+        new_list.append(num)
+
+print("Original list:", numbers)
+print("Updated list:", new_list)
