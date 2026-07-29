ee24d8505ec4ca9325a11ce5dd8ff83d795f7466
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-07-29 15:26:16 +0530

    Practice program to print the following pattern

--- Revision_Programs/Programs.py
+++ Revision_Programs/Programs.py
@@ -1265,3 +1265,22 @@ for i in range(1, 200):
 
 
 print(f"Your answer is {total}")
+
+
+"""
+Write a program to print the following pattern 
+
+1 
+2 2 
+3 3 3 
+4 4 4 4 
+5 5 5 5 5 
+
+"""
+
+for i in range(1, 6):
+
+    for j in range(1, i + 1):
+        print(i, end=" ")
+
+    print()
