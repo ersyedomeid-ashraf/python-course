7ef68eb215baf97b4034428cfce7726aaedc4555
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-07-29 15:27:42 +0530

    Practice program to print all the elements present at even indec position

--- Revision_Programs/Programs.py
+++ Revision_Programs/Programs.py
@@ -1284,3 +1284,17 @@ for i in range(1, 6):
         print(i, end=" ")
 
     print()
+
+
+"""
+Write a program to make your own list. Print all the elements present at even index position.
+"""
+
+my_list = [21, 43, 45, 32, "Hurray", 89, 76, "Done", "Done"]
+
+
+# Iteration by index
+
+for i in range(0, len(my_list)):
+    if i % 2 == 0:
+        print(my_list[i], end=" ")
