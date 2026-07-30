54769bca9ceaaa456ac32a61d33c0ed5122bb90a
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-07-30 19:36:28 +0530

    Practice program to find common elements in three lists using sets

--- Revision_Programs/Programs.py
+++ Revision_Programs/Programs.py
@@ -1425,3 +1425,39 @@ for name, marks in students_data.items():
     total = sum(marks)
     percentage = total / 500 * 100
     print(f" {name} has scored total {total} marks, percentage = {percentage:.2f}")
+
+
+"""
+Write a program to find common elements in three lists using sets.
+"""
+
+list1 = [
+    45,
+    3,
+    4,
+    4,
+    5,
+    56,
+    "laila",
+    67,
+    76,
+    "Kaaalu",
+    4,
+    3,
+    4556,
+    "tinaa",
+    654,
+    767,
+    678,
+]
+list2 = [4, 5, 654, 45, 789, 78, "Kaaalu", 899, 87, 667, 66, 55, "laila"]
+list3 = [5, 4, 3, "laila", "tina", 45, 4556, "Kaaalu", 654, 767]
+
+set1 = set(list1)
+set2 = set(list2)
+set3 = set(list3)
+
+x = set1.intersection(set2)
+result = x.intersection(set3)
+
+print(result)
