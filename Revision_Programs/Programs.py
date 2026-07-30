8950086dd87f23711874e3dc08839c28d58445bb
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-07-30 15:58:13 +0530

    Practice program to copy all the elements in another list but in reverse order

--- Revision_Programs/Programs.py
+++ Revision_Programs/Programs.py
@@ -1334,3 +1334,21 @@ for i in my_list:
 
 
 print(total)
+
+
+"""
+Write a program to take 10 integer input from user and store them in a list. Now, copy all
+the elements in another list but in reverse oreder
+"""
+
+my_list = []
+for i in range(5):
+    value = int(input("Enter value at index {i} = "))
+    my_list.append(value)
+
+
+result = []
+
+for i in range(len(my_list) - 1, -1, -1):
+    result.append(my_list[i])
+print(result)
