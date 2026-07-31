946463e7590103defe94e188d7ff89d914203dc4
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-07-31 23:12:08 +0530

    Practice program to remove all elements from a given sets

--- Revision_Programs/Programs.py
+++ Revision_Programs/Programs.py
@@ -1618,3 +1618,32 @@ for i in range(1, 6):
         print(j, end=" ")
 
     print()
+
+
+"""
+Write a program to remove all elements from a given sets.
+"""
+
+my_set = {34, 54, 46, 78, 98}
+
+my_set.clear()
+print(my_set)
+
+
+my_set = {34, 32, 56, 78, 98, 766, 778, 90}
+
+my_set.clear()
+print(my_set)
+
+
+my_set = {33, 45, 90, 98, 77, 67, 68, 78, 98, 56, 4556, 778887, 544, 5678, 889}
+
+my_set.clear()
+print(my_set)
+
+
+my_set = {34, 34, 32, 34, 332, 44, 32, 44}
+
+
+my_set.clear()
+print(my_set)
