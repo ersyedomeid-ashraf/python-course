5a1b0b1333ec909300258f1d989ada04f4590626
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-07-31 23:05:24 +0530

    Practice program to print all the even numbers present in the list

--- Revision_Programs/Programs.py
+++ Revision_Programs/Programs.py
@@ -1539,3 +1539,23 @@ def largest(num1, num2, num3):
 
 
 largest(233, 465, 421)
+
+
+"""
+Write a program to make your own list. Print all the even numbers present in the list.
+"""
+
+my_list = [54, 65, 34, 34, 56, 67, 87, 85, 90, 98]
+
+
+# Iteration by index
+for i in range(0, len(my_list)):
+    if i % 2 == 0:
+        print(my_list[i], end=" ")
+
+
+# Iteration by value
+
+for i in my_list:
+    if i % 2 == 0:
+        print(i, end=" ")
