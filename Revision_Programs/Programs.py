6478241e866ad58d27a10aec1ecb920691abde5e
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-08-02 23:06:54 +0530

    Practice default parameter function program

--- Revision_Programs/Programs.py
+++ Revision_Programs/Programs.py
@@ -1801,3 +1801,21 @@ print(my_dict)
 
 my_dict.popitem()
 print(my_dict)
+
+
+# Default Parameters
+
+
+def total_marks(physics=0, chemistry=0, biology=0, maths=0, english=0):
+
+    print(f"Your marks in physics = {physics}")
+    print(f"Your marks in chemistry = {chemistry}")
+    print(f"Your marks in biology = {biology}")
+    print(f"Your marks in maths = {maths}")
+    print(f"Your marks in english = {english}")
+
+    total = physics + chemistry + biology + maths + english
+    print(f"Your total marks = {total}")
+
+
+total_marks(english=99, maths=96)
