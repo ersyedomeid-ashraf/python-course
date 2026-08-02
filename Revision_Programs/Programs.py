276e40a6e236e172f490a953026c0b5a9a618e3d
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-08-02 23:07:41 +0530

    Practice named parameter function program

--- Revision_Programs/Programs.py
+++ Revision_Programs/Programs.py
@@ -1819,3 +1819,36 @@ def total_marks(physics=0, chemistry=0, biology=0, maths=0, english=0):
 
 
 total_marks(english=99, maths=96)
+
+
+def total_marks(physics, chemistry, biology, maths, english):
+
+    print(f"Your marks in physics = {physics}")
+    print(f"Your marks in chemistry = {chemistry}")
+    print(f"Your marks in biology = {biology}")
+    print(f"Your marks in maths = {maths}")
+    print(f"Your marks in english = {english}")
+
+    total = physics + chemistry + biology + maths + english
+    print(f"Your total marks = {total}")
+
+
+total_marks(64, 78, 86, 98, 90)
+
+
+# Now we used Named Parameter
+
+
+def total_marks(physics, chemistry, biology, maths, english):
+
+    print(f"Your marks in physics = {physics}")
+    print(f"Your marks in chemistry = {chemistry}")
+    print(f"Your marks in biology = {biology}")
+    print(f"Your marks in maths = {maths}")
+    print(f"Your marks in english = {english}")
+
+    total = physics + chemistry + biology + maths + english
+    print(f"Your total marks = {total}")
+
+
+total_marks(chemistry=88, maths=90, english=99, biology=96, physics=80)
