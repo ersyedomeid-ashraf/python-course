79dedfeb0ddfbb47c70847d5d2607bcf16fbea20
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-08-03 13:41:07 +0530

    Practice netsed loop program

--- Revision_Programs/Programs.py
+++ Revision_Programs/Programs.py
@@ -1965,3 +1965,18 @@ for i in range(20, 45):
     print("done")
 
 print("Program Finish")
+
+
+# Nested Loops
+
+for i in range(1, 4):
+    print(i)
+    for j in range(8, 12):
+        print(j)
+
+
+for i in range(1, 8):
+    print(i)
+
+for j in range(14, 18):
+    print(j)
