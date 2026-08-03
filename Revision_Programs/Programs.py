ea4d70a398b5769dca58551ed545ab55a4a0f1a9
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-08-03 13:42:38 +0530

    Practice methods in sets program

--- Revision_Programs/Programs.py
+++ Revision_Programs/Programs.py
@@ -1980,3 +1980,22 @@ for i in range(1, 8):
 
 for j in range(14, 18):
     print(j)
+
+
+my_set1 = {43, 45, 44, 43, 45, 56, 76, 82, 46, 76}
+my_set2 = {45, 89, 76, 87, 667, 885, 990}
+
+print(my_set1.union(my_set2))
+print(my_set1.intersection(my_set2))
+
+
+# list common example
+
+a = [1, 2, 3, 4, 5]
+b = [4, 5, 6, 7, 8, 9]
+
+c = set(a)
+d = set(b)
+
+result = list(c.intersection(d))
+print(result)
