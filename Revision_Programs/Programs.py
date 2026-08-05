0f0ecedd4d826936dbce38473d9131c871ba719a
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-08-05 22:56:00 +0530

    Practice palindrome program

--- Revision_Programs/Programs.py
+++ Revision_Programs/Programs.py
@@ -2122,3 +2122,21 @@ print(c)
 
 c = my_string.endswith("the")
 print(c)
+
+
+my_string = input("Enter a string = ")
+
+if my_string == my_string[::-1]:
+    print("It is a palindrome")
+
+else:
+    print("It is not a palindrome")
+
+
+my_string = input("Enter a string = ")
+
+if my_string == my_string[::-1]:
+    print("It is a palindrome")
+
+else:
+    print("It is not a palindrome")
