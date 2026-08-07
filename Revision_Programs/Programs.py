2e55e37fb301082ec5a372f4c2d773626912049e
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-08-07 19:13:41 +0530

    Practice program to check element presence using count()

--- Revision_Programs/Programs.py
+++ Revision_Programs/Programs.py
@@ -2227,3 +2227,23 @@ a = [43, 55, 78, 77, 89, 90, 34, 67]
 
 b = a[:4]
 print(b)
+
+
+# Ask a number from user
+# Print yes if number exists in list else print no
+
+num = int(input("Enter a number = "))
+if num in a:
+    print("Yes")
+
+else:
+    print("No")
+
+
+# Another way to check element presence using count()
+
+num = int(input("Enter a number = "))
+if a.count(num) > 0:
+    print("Yes")
+else:
+    print("No")
