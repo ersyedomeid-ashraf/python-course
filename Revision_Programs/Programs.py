f1fac7d83251544017341c4050b25d100d8e5e5d
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-08-08 16:40:04 +0530

    Practice program to check given number is positive, negative or equal ot zero

--- Revision_Programs/Programs.py
+++ Revision_Programs/Programs.py
@@ -2290,3 +2290,33 @@ square(num)
 my_tuple = (34, 56, 78, 34, 97, 56, 34)
 for i in my_tuple:
     print(i)
+
+
+"""
+Write a program to check whether a given number is positive, negative or equal to zero. 
+"""
+# This is method 1 :
+num = int(input("Enter a number = "))
+
+if num >= 0:
+    if num > 0:
+        print("Postive")
+
+    else:
+        print("Equal to zero ")
+
+else:
+    print("Negative")
+
+
+# This is method 2 :
+
+if num > 0:
+    print("Positive")
+
+else:  # <=0
+    if num == 0:
+        print("Equal to zero")
+
+    else:
+        print("Negative")
