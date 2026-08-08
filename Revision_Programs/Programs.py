49001732190311d79c8829756c82b84bdfc3d799
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-08-08 16:44:01 +0530

    Practice program to print the sum of all the elements present in the list

--- Revision_Programs/Programs.py
+++ Revision_Programs/Programs.py
@@ -2369,3 +2369,30 @@ words = words[::-1]
 
 result = " ".join(i for i in words)
 print(result)
+
+
+"""
+Write a program to make your own list. Print the sum of all the elements present in the list.
+"""
+
+my_list = [21, 32, 43, 54, 65, 76, 87, 98, 202]
+
+# Iteration by index
+
+total = 0
+for i in range(0, len(my_list)):
+    total = total + my_list[i]
+
+print(total)
+
+
+# Iteration by value
+
+my_list = [21, 32, 43, 54, 65, 76, 87, 98, 202]
+
+total = 0
+
+for i in my_list:
+    total = total + i
+
+print(total)
