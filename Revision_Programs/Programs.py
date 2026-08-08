c98dfca51557158a71abd915e67d09ef880d957d
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-08-08 16:42:05 +0530

    Practice program to merge the number from the first second list together

--- Revision_Programs/Programs.py
+++ Revision_Programs/Programs.py
@@ -2333,3 +2333,24 @@ if num % 2 == 0 and num % 3 == 0 and num % 8 != 0:
 
 else:
     print("No")
+
+
+"""
+Write a program to start by creating two seperate lists with random numbers. Then, create  third list
+that merges he numbers from he first second lists together.
+"""
+
+list1 = [1, 3, 5, 50, 68]
+list2 = [3, 5, 90, 65, 76]
+
+result = []
+
+for num in list1:
+    result.append(num)
+
+
+for num in list2:
+    result.append(num)
+
+
+print(result)
