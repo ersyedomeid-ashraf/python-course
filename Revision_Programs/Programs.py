29e25109bf7c0e5edf73d42a873997e2be9a2697
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-08-09 17:47:07 +0530

    Practice program to kep editing it to dictionary

--- Revision_Programs/Programs.py
+++ Revision_Programs/Programs.py
@@ -2413,3 +2413,21 @@ else:
     bill = 100 * 5 + 100 * 7 + (units - 200) * 10
 
 print("Bill:", bill)
+
+
+"""
+Write a program to ask subject name and marks from the user and keep
+editing it to dictionary.
+"""
+
+marks = {}
+
+subject_count = int(input("Enter how many subjects = "))
+
+for _ in range(0, subject_count):
+    subject_name = input("Enter subject name = ")
+    subject_marks = int(input(f"Enter marks for {subject_name} ="))
+    marks[subject_name] = subject_marks
+    # marks.update({subject_name: subject_marks})        # you can write this also
+
+print(marks)
