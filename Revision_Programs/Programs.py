14f7b6c29d71b8d8a1d19dd9e1d63e6d77b77850
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-08-10 13:47:18 +0530

    Practice program to find the average of all the numbers present in the list

--- Revision_Programs/Programs.py
+++ Revision_Programs/Programs.py
@@ -2556,3 +2556,23 @@ n = len(my_list)
 my_list[0], my_list[n - 1] = my_list[n - 1], my_list[0]
 
 print(my_list)
+
+
+"""
+Write a program to find the average of all the numbers present in the list.
+"""
+
+my_list = [56, 68, 100, 5, 90, 556]
+
+average = sum(my_list) / len(my_list)
+
+print("Average =", average)
+
+
+# Another one
+
+my_list = [22, 32, 45, 67, 87, 89, 90, 23]
+
+average = sum(my_list) / len(my_list)
+
+print("Average =", average)
