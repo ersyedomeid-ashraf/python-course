3ebea28a228736a2cd644c756c3ff751b4b25310
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-08-10 13:45:35 +0530

    Practice program to prints whether it is odd or even

--- Revision_Programs/Programs.py
+++ Revision_Programs/Programs.py
@@ -2526,3 +2526,20 @@ for i in my_set:
     count += 1
 
 print(count)
+
+
+"""
+Write a program to write a function that accepts an integer and prints whether it is odd or even
+"""
+
+
+def even_odd(num):
+    if num % 2 == 0:
+        print("It is a even number")
+
+    else:
+        print("It is a odd number")
+
+
+even_odd(10)
+even_odd(9)
