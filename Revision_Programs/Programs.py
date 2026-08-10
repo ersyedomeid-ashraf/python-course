0ca8116c0e01725ec379ba30f01ce5128643b7da
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-08-10 22:28:18 +0530

    Practice program to print the following pattern

--- Revision_Programs/Programs.py
+++ Revision_Programs/Programs.py
@@ -2592,3 +2592,24 @@ for i in range(0, len(my_list)):
 
 
 print(largest)
+
+
+"""
+Write a program to print the following pattern 
+       
+         * 
+      * * *
+    * * * * *
+  * * * * * * *
+* * * * * * * * * 
+
+"""
+
+for i in range(1, 6):
+    for j in range(i, 5):
+        print(" ", end=" ")
+
+    for k in range(1, (i * 2)):
+        print("*", end=" ")
+
+    print()
