0f7f7dbc1303483ab7e0d6eeeb90c5fe7f7d2258
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-08-11 18:22:54 +0530

    Practice program to print the sum and average of these numbers

--- Revision_Programs/Programs.py
+++ Revision_Programs/Programs.py
@@ -2660,3 +2660,26 @@ end = int(input("Enter a end number = "))
 
 for i in range(start, end + 1):
     print(i, end=" ")
+
+
+"""
+Write a program to write a funtion that takes a list of number and prints the
+sum and average of these numbers.
+"""
+
+
+def sum_avg_lst(lst):
+
+    total = 0
+
+    for num in lst:
+        total = total + num
+
+    print(f"Total of all numbers = {total}")
+
+    avg = total / len(lst)
+    print(f"Average of all numbers = {avg}")
+
+
+sum_avg_lst([1, 2, 3, 4, 5])
+sum_avg_lst([59, -86, 74, 102, 97])
