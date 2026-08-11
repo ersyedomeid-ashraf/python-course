f235a2399b2df4ad3fcf1bb9818a5b4b60d3325b
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-08-11 18:25:34 +0530

    Practice program to prints the largest among them

--- Revision_Programs/Programs.py
+++ Revision_Programs/Programs.py
@@ -2706,3 +2706,27 @@ print(result)
 # in short way we'll write
 
 print(set(list1) & set(list2) & set(list3))
+
+
+"""
+Write a program to write a function that takes three number as a parameter
+and prints the largest among them.
+"""
+
+
+def largest(num1, num2, num3):
+
+    if num1 > num2 and num1 > num3:
+        print("num1")
+        print(f"{num1} is the largest")
+
+    elif num2 > num1 and num2 > num3:
+        print(num2)
+        print(f"{num2} is the largest")
+
+    else:
+        print("num3")
+        print(f"{num3} is the largest")
+
+
+largest(9898, 8989, 9988)
