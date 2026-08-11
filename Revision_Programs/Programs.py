f987b9a8c46c502edac792f091c90a964f1f82d4
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-08-11 18:22:09 +0530

    Practice program to print all the numbers from start to end

--- Revision_Programs/Programs.py
+++ Revision_Programs/Programs.py
@@ -2647,3 +2647,16 @@ print(f"games_ties = {games_tie}")
 
 Total_points = (games_won * 4) + (games_lost * 2)
 print(Total_points)
+
+
+"""
+Write a program to ask a start number and end number from user. print all the numbers 
+from start to end. 
+"""
+
+
+start = int(input("Enter a start number = "))
+end = int(input("Enter a end number = "))
+
+for i in range(start, end + 1):
+    print(i, end=" ")
