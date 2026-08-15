a7e8c8f1b6166d92b8e187fafedd6a4829476a7c
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-08-15 09:55:46 +0530

    Practice program to calculate sum of 5 subjects and find percentage

--- Revision_Programs/Programs.py
+++ Revision_Programs/Programs.py
@@ -2892,3 +2892,19 @@ f = float(input("Enter temprature in Fahrenheit = "))
 c = (f - 32) * 5 / 9
 
 print(f"{f} Fahrenheit = {c} Celsius ")
+
+
+"""
+Calculate sum of 5 subjects and Find percentage
+"""
+
+
+English1 = int(input("Enter Englsh 1 = "))
+Hindi2 = int(input("Enter Hindi 2 = "))
+Maths3 = int(input("Enter Maths 3 = "))
+Science4 = int(input("Enter Science 4 = "))
+Computer5 = int(input("Enter Computer 5 = "))
+
+percentage = (English1 + Hindi2 + Maths3 + Science4 + Computer5) / 5
+
+print(f"Percentage, {percentage}")
