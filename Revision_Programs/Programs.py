253994e79671d44d91b2307292b3aca1db448bf7
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-08-15 09:51:09 +0530

    Practice program to convert float to an integer and vice versa

--- Revision_Programs/Programs.py
+++ Revision_Programs/Programs.py
@@ -2860,3 +2860,23 @@ num3 = int(input("Enter number 3 = "))
 average = (num1 + num2 + num3) / 3
 
 print(f"Average, {average}")
+
+
+"""
+Convert a float to an integer and vice versa.
+"""
+
+
+number_float = 55.5
+print(number_float, type(number_float))
+
+number = int(number_float)
+print(number, type(number))
+
+# Now we do it vice versa
+
+number = int(number_float)
+print(number, type(number))
+
+number_float = 66.6
+print(number_float, type(number_float))
