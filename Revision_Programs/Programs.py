548523666734b85989d9667c98ea857897d568da
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-08-16 18:00:40 +0530

    Practice program to check if the number is odd, even or ewual to zero

--- Revision_Programs/Programs.py
+++ Revision_Programs/Programs.py
@@ -2937,3 +2937,19 @@ if num1 % num2 == 0:
 
 else:
     print("No, it's not divisible by 2")
+
+
+"""
+Write a program to check if the number is ODD, EVEN or EQUAL to Zero 
+"""
+
+num = int(input("Enter a number = "))  # In Python 0%2 = 0
+
+if num == 0:
+    print("It is equal to zero")
+
+elif num % 2 == 1:
+    print("ODD")
+
+else:
+    print("EVEN")
