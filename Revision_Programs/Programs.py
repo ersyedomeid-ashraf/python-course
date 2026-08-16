0d7d1cac1ef4c0d9e4af5d5a4bb1a872863e2812
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-08-16 22:34:33 +0530

    Practice program to print the last digit of a number

--- Revision_Programs/Programs.py
+++ Revision_Programs/Programs.py
@@ -2966,3 +2966,14 @@ if num % 2 == 0 and num % 3 == 0 and num % 8 != 0:
 
 else:
     print("No")
+
+
+"""
+Write a program to print the last digit of a number. 
+Example -> Input = 12345 Output = 5.
+"""
+
+num = int(input("Enter a number = "))
+
+last_digit = num % 10
+print(f"The last digit of {num} is {last_digit}")
