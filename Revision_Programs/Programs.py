23f6de0fa1f2f29a248c3f5ac1e5a06b9fc95e01
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-08-16 17:59:26 +0530

    Practice program to check if the first number is divisible by the second number

--- Revision_Programs/Programs.py
+++ Revision_Programs/Programs.py
@@ -2922,3 +2922,18 @@ if char == "a" or char == "e" or char == "i" or char == "o" or char == "u":
 
 else:
     print("Consonant")
+
+
+"""
+Write a number that takes two numbers as input and checks if the 
+first number is divisible by the second number.
+"""
+
+num1 = int(input("Enter a number 1 = "))
+num2 = int(input("Enter a number 2 = "))
+
+if num1 % num2 == 0:
+    print("yes, it's divisible by 2")
+
+else:
+    print("No, it's not divisible by 2")
