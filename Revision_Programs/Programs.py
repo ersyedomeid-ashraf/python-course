49bf416546d0eac933fcf0baeccd7c2a37a6dd5f
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-08-16 22:35:48 +0530

    Practice program to check if the last digit of a number is divisible by 5 or not

--- Revision_Programs/Programs.py
+++ Revision_Programs/Programs.py
@@ -2977,3 +2977,18 @@ num = int(input("Enter a number = "))
 
 last_digit = num % 10
 print(f"The last digit of {num} is {last_digit}")
+
+
+"""
+Write a program to check if the last digit of a number is divisible by 5 or not. 
+"""
+
+num = int(input("enter a number = "))
+
+last_digit = num % 10
+
+if last_digit == 0 or last_digit == 5:
+    print("Yes, it is divisible by 5")
+
+else:
+    print("No, it is not divisible by 5")
