a12c918a108d70a55674f8dd0261977a886465fa
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-08-17 11:23:33 +0530

    Practice program to print which number is the smallest

--- Revision_Programs/Programs.py
+++ Revision_Programs/Programs.py
@@ -3029,3 +3029,26 @@ print(f"You got {discount}% discount")
 
 final_bill = bill_amount - (bill_amount * discount / 100)
 print(f"Your final bil is {final_bill}")
+
+
+"""
+Write a program Ask 4 numbers from the user. Make sure all the numbers entered by user 
+are different. Print which number is the samllest. 
+"""
+
+num1 = int(input("Enter a number 1  = "))
+num2 = int(input("Enter a number 2 = "))
+num3 = int(input("Enter a number 3 = "))
+num4 = int(input("Enter a number 4 = "))
+
+if num1 < num2 and num1 < num3 and num1 < num4:
+    print("num1 is the smallest")
+
+elif num2 < num1 and num2 < num3 and num2 < num4:
+    print("num2 is the smallest")
+
+elif num3 < num1 and num3 < num2 and num3 < num4:
+    print("num3 is the smallest")
+
+else:
+    print("num4 is the smallest")
