b249eaf5f31953615b98d0d1034ea3c570907c92
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-08-17 11:25:42 +0530

    Practice program to all the number from 1 to that number

--- Revision_Programs/Programs.py
+++ Revision_Programs/Programs.py
@@ -3077,3 +3077,17 @@ elif num % 5 == 0:
 
 else:
     print(num)
+
+
+"""
+Write a program to ask a number from user. Print all the number from 1 to that number.
+"""
+
+
+num = int(input("Enter a number = "))
+
+i = 1
+while i <= num:
+    print(i)
+
+    i = i + 1
