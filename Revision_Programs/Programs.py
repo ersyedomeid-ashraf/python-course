75a914b2d2e5d582d56bbae6bfdbb09f53b9d65d
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-08-18 23:33:27 +0530

    Practice program to calculate the sum of all the numbers from 1 to 100

--- Revision_Programs/Programs.py
+++ Revision_Programs/Programs.py
@@ -3116,3 +3116,18 @@ while i >= 1:
     print(i, end=" ")
 
     i = i - 1
+
+
+"""
+Write a program to calculate the sum of all the numbers from 1 to 100.
+"""
+
+i = 1
+total = 0  # you can choose any variable name. Here, we use "total"
+
+while i <= 100:
+    total = total + i
+    i = i + 1
+
+
+print(total)
