cea84a2769d2ce75f9402a7d41b1eb0f9b1002e9
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-08-18 21:26:10 +0530

    practice program to print all the numbers from n to 1

--- Revision_Programs/Programs.py
+++ Revision_Programs/Programs.py
@@ -3102,3 +3102,17 @@ print(name)
 print(age)
 print(gender)
 print(city)
+
+
+"""
+Write a program to ask a number (N) from user. Print all the numbers from N to 1. 
+"""
+
+
+num = int(input("Enter a number = "))
+
+i = num
+while i >= 1:
+    print(i, end=" ")
+
+    i = i - 1
