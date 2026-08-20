713d421fc27dcbe773f9e0cedf339a99692ed4ca
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-08-20 20:00:35 +0530

    Practice program to calculate the sum of all the numbers from 1 to 200

--- Revision_Programs/Programs.py
+++ Revision_Programs/Programs.py
@@ -3269,3 +3269,15 @@ for i in range(1, 11):
 
 
 print(f"Your answer is {total}")
+
+
+# Calculate the sum of all the number from 1 to 200.
+
+
+total = 0
+
+for i in range(1, 200):
+    total = total + i
+
+
+print(f"Your answer is {total}")
