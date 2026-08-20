f47e3a0abac1062b1353b9612cf5aab2643db7e9
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-08-20 19:57:46 +0530

    Practice program to print all the numbers from that number to 1

--- Revision_Programs/Programs.py
+++ Revision_Programs/Programs.py
@@ -3232,3 +3232,13 @@ Write a program to ask a number from the user. print all the numbers from 1 to t
 num = int(input("Enter a number = "))
 for i in range(1, num + 1):
     print(i, end=" ")
+
+
+"""
+Write a program to ask a number from the user. print all the numbers from that number to 1.
+"""
+
+num = int(input("Enter a number = "))
+
+for i in range(num, 0, -1):
+    print(i)
