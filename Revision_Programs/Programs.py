eabad59d9c67ec751c7d19fcfedc5e2a9ed5f8d3
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-08-22 23:23:37 +0530

    Practice program to print which is greatest

--- Revision_Programs/Programs.py
+++ Revision_Programs/Programs.py
@@ -3308,3 +3308,20 @@ while i <= 200:
 
 
 print(count)
+
+
+"""
+Ask two number from user, print which is greatest.
+"""
+
+num1 = int(input("Enter number 1 = "))
+num2 = int(input("Enter number 2 = "))
+
+if num1 > num2:
+    print("Num1 is greatest")
+
+elif num2 > num1:
+    print("Num2 is greatest")
+
+else:
+    print("Both are equal")
