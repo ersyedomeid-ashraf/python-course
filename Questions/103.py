0f5065575b819d0f27cfa0014e159066282ce028
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-08-23 23:42:57 +0530

    Implement program to prints whether it is a palindrome

--- Questions/103.py
+++ Questions/103.py
@@ -16,3 +16,22 @@ def check_palindrome(string):
 check_palindrome("mom")
 check_palindrome("anaa")
 check_palindrome("madam")
+
+
+# Another one
+
+
+def check_palindrome(string):
+
+    if string == string[::-1]:
+        print("It is a palindrome")
+
+    else:
+        print("It is not a palindrome")
+
+
+check_palindrome("kuchu puchu")
+check_palindrome("civic")
+check_palindrome("refer")
+check_palindrome("anaaa")
+check_palindrome("rotator")
