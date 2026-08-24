65983985582b1c5f685a50da1f14616437a62fff
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-08-24 17:34:53 +0530

    Implement integer value data type program

--- Basics/tut6-Data_types.py
+++ Basics/tut6-Data_types.py
@@ -1,32 +1,38 @@
 # int = integer values
 x = 65
 print(x)
-print(type(x))                              
+print(type(x))
 
 # int = integer values
 x = 72
-print(x,type(x))
+print(x, type(x))
 y = 23.4
 
 # float = decimal values
-print(y,type(y))
+print(y, type(y))
 
 # str = string/text values
 name = "Syed"
-print(name,type(name)) 
+print(name, type(name))
 
-# bool = True or False values 
+# bool = True or False values
 adult = True
-print(adult,type(adult))
+print(adult, type(adult))
 
 # list = multiple values inside square brackets []
-marks = [33,44,78,80,77]
-print(marks,type(marks))
+marks = [33, 44, 78, 80, 77]
+print(marks, type(marks))
 
 # tuple = multiple values inside parenthesis ()
-marks = (80,77,50,65,36)
-print(marks,type(marks))
+marks = (80, 77, 50, 65, 36)
+print(marks, type(marks))
 
 # dictionary = key-value pairs inside curly braces {}
-marks = {"Syed":70,"Ayush":35,"Ambani":19}
-print(marks,type(marks))
+marks = {"Syed": 70, "Ayush": 35, "Ambani": 19}
+print(marks, type(marks))
+
+
+# int = integer values
+x = 65
+print(x)
+print(type(x))
