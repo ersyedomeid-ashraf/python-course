bf048a182bcb032576bcf9d918d394aad8d15544
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-08-27 19:51:27 +0530

    Implement program to ad numbers

--- Basics/tut9-add_number_input.py
+++ Basics/tut9-add_number_input.py
@@ -14,3 +14,16 @@ num3 = int(input("Enter third number = "))
 
 total = num1 + num2 + num3
 print(f"Your total is = {total}")
+
+
+num1 = int(input("Enter first number = "))
+num2 = int(input("Enter second number = "))
+num3 = int(input("Enter third number = "))
+num4 = int(input("Enter fourth number = "))
+num5 = int(input("Enter fifth number = "))
+num6 = int(input("Enter third number = "))
+num7 = int(input("Enter fourth number = "))
+num8 = int(input("Enter fifth number = "))
+
+total = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8
+print(f"Your total is = {total}")
