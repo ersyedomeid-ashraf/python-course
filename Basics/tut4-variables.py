d91306080a6baf326d596a1ad42861419853a6f8
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-08-29 20:32:14 +0530

    Implement variables program

--- Basics/tut4-variables.py
+++ Basics/tut4-variables.py
@@ -1,6 +1,6 @@
 x = 23
 y = 13
-z = x+y
+z = x + y
 
 print(x)
 print(y)
@@ -14,19 +14,30 @@ city = "Bareilly"
 print(name)
 print(age)
 print(gender)
-print(city) 
+print(city)
 
-__=39
-____=48
-_____=11
-print(__+____+_____)
+__ = 39
+____ = 48
+_____ = 11
+print(__ + ____ + _____)
 
 firstname = "Syed"
 print(firstname)
 
-# you can write in this form also 
+# you can write in this form also
 
 first_name = "Syed"
 mid_name = "Omeid"
 last_name = "Ashraf"
-print(first_name,mid_name,last_name)
+print(first_name, mid_name, last_name)
+
+
+# Variables revision
+
+x = 23
+y = 13
+z = x + y
+
+print(x)
+print(y)
+print(z)
