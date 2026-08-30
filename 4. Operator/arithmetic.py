c2f6b322254c24a68d1511057cebfbb013ac552a
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-08-30 21:34:43 +0530

    Implement arithmetic operator addition program

--- 4. Operator/arithmetic.py
+++ 4. Operator/arithmetic.py
@@ -1,17 +1,24 @@
+# Arithmetic Operators
 
-# Arithmetic Operators 
 
-
-a = 10 
+a = 10
 b = 5
 
-print(a+b)
-print(a-b)
-print(a*b)
-print(a/b)
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
 
 
 print(a**b)
-print(a%b)
-print(a/b)
-print(a//b)
+print(a % b)
+print(a / b)
+print(a // b)
+
+
+# Arithmetic Operators. Revision
+
+a = 56
+b = 456
+
+print(a + b)
