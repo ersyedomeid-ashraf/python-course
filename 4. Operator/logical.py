873f316498535588bf489314a104e4f539098b89
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-09-01 20:46:12 +0530

    Implement logical operator porgram

--- 4. Operator/logical.py
+++ 4. Operator/logical.py
@@ -50,3 +50,12 @@ print(physics > 33 or chemistry > 33)
 
 print(physics > 33)
 print(not physics > 33)
+
+
+english = 54
+mathematic = 88
+science = 90
+
+print(english > 33 and mathematic > 33 and science > 33)
+print(english > 33 or mathematic > 33 or science > 33)
+print(not english > 33, not mathematic > 33, not science > 33)
