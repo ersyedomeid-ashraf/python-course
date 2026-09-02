7a639ab88e44c554ce1edf754d270270bcf74a78
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-09-02 23:44:52 +0530

    Implement program to print pass if student is passed in both subjects else print fail

--- Conditional Statement/p3.py
+++ Conditional Statement/p3.py
@@ -1,14 +1,31 @@
 """
-Ask physics and chemistry marks from user 
-Print PASS, if student is passed in both subjects 
+Ask physics and chemistry marks from user
+Print PASS, if student is passed in both subjects
 Else print FAIL
 """
 
 Physics = int(input("Enter Physics marks = "))
 Chemistry = int(input("Enter Chemistry marks = "))
 
-if Physics >= 33 and Chemistry >= 33 :
+if Physics >= 33 and Chemistry >= 33:
     print("PASS")
 
-else :
+else:
+    print("FAIL")
+
+
+# Revision
+"""
+Ask physics and chemistry marks from user 
+Print PASS, if student is passed in both subjects 
+Else print FAIL
+"""
+
+Biology = int(input("Enter Biology marks = "))
+Mathematics = int(input("Enter Mathematics marks = "))
+
+if Biology >= 33 and Mathematics >= 33:
+    print("PASS")
+
+else:
     print("FAIL")
