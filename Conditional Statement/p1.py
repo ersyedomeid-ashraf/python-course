d88eb1c508aa79e6ce3352a54504d45708546bc4
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-09-02 23:33:14 +0530

    Implement program to print number which is the greatest

--- Conditional Statement/p1.py
+++ Conditional Statement/p1.py
@@ -1,4 +1,20 @@
 """
+Ask 2 numbers from user
+Print number which is the greatest
+"""
+
+num1 = int(input("Enter number 1 = "))
+num2 = int(input("Enter number 2 = "))
+
+if num1 > num2:
+    print("Number 1 is greater")
+
+else:
+    print("Number 2 is greater")
+
+
+# Revision
+"""
 Ask 2 numbers from user 
 Print number which is the greatest
 """
@@ -6,9 +22,8 @@ Print number which is the greatest
 num1 = int(input("Enter number 1 = "))
 num2 = int(input("Enter number 2 = "))
 
-if num1 > num2 :
+if num1 > num2:
     print("Number 1 is greater")
 
-else :
+else:
     print("Number 2 is greater")
-
