c3bc08c2760b653538605e4e4ccea4fa50ce2c5e
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-09-03 10:35:30 +0530

    Implement if else conditional statement program

--- Conditional Statement/if-else.py
+++ Conditional Statement/if-else.py
@@ -1,14 +1,34 @@
 # Conditional Statement
 
-# Ask age from user 
+# Ask age from user
 # you can vote if 18 above
 
 age = int(input("Enter your age = "))
 
-if age >= 18 :
-    print("You can vote")                         #In the beginning the space is called as indentation.
-    print("You are allowed to vote")              #Indentation refers to the space at the beginning of a code line.
+if age >= 18:
+    print("You can vote")  # In the beginning the space is called as indentation.
+    print(
+        "You are allowed to vote"
+    )  # Indentation refers to the space at the beginning of a code line.
 
-else :
-    print("You cannot vote")                      #In the beginning the space is called as indentation.
-    print("You are not allowed to vote")          #Indentation refers to the space at the beginning of a code line.
+else:
+    print("You cannot vote")  # In the beginning the space is called as indentation.
+    print(
+        "You are not allowed to vote"
+    )  # Indentation refers to the space at the beginning of a code line.
+
+
+# Conditional Statement Revision
+
+# Ask age from user
+# you can vote if 18 above
+
+age = int(input("Enter your age = "))
+
+if age >= 18:
+    print("You can vote")
+    print("You are allowed to vote")
+
+else:
+    print("You cannot vote")
+    print("You are not allowed to vote")
