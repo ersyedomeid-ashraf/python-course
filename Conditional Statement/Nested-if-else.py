408b493b4f3a96d1a7f575c643f8ff174bd81a5b
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-09-03 11:19:43 +0530

    Implement program to check whether a given number is positive, negative or equal to zero

--- Conditional Statement/Nested-if-else.py
+++ Conditional Statement/Nested-if-else.py
@@ -1,30 +1,46 @@
 """
-Write a program to check whether a given number is positive, negative or equal to zero. 
+Write a program to check whether a given number is positive, negative or equal to zero.
 """
+
 # This is method 1 :
 num = int(input("Enter a number = "))
 
-if num >= 0 :
-    if num > 0 :
+if num >= 0:
+    if num > 0:
         print("Postive")
-    
-    else :
+
+    else:
         print("Equal to zero ")
 
-else :
+else:
     print("Negative")
 
 
 # This is method 2 :
 
-if num > 0 :
+if num > 0:
     print("Positive")
 
-else :   # <=0
-    if num == 0 :
+else:  # <=0
+    if num == 0:
         print("Equal to zero")
 
-    else :
-        print("Negative") 
-           
-  
+    else:
+        print("Negative")
+
+
+"""
+Write a program to check whether a given number is positive, negative or equal to zero. 
+"""
+# This is method 1 :
+num = int(input("Enter a number = "))
+
+if num >= 0:
+    if num > 0:
+        print("Postive")
+
+    else:
+        print("Equal to zero ")
+
+else:
+    print("Negative")
