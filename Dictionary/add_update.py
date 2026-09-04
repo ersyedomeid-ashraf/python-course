b372c8ef62af112fc43b8078a1352e6a6f103b5e
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-09-04 15:05:11 +0530

    Implement add update dictionary program

--- Dictionary/add_update.py
+++ Dictionary/add_update.py
@@ -40,3 +40,25 @@ my_dict = {
 }
 
 print(my_dict)
+
+
+# Method 1
+
+# Create a dictionary and update the value of an existing key.
+
+my_dict["age"] = 67
+print(my_dict)
+
+
+# Add a new key-value pair to an existing dictionary and print the updated dictionary.
+
+my_dict["abc"] = 99
+print(my_dict)
+
+
+# Method 2
+
+# Update multiple values and add a new key to a dictionary using the update() method.
+
+my_dict.update({"marks": 89, "address": "Jharkhand", "Name": "ABC"})
+print(my_dict)
