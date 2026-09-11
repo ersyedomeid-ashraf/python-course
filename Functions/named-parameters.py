65c590c7c4bc85ed6aa1de49de27806647bfa3fe
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-09-11 17:30:37 +0530

    Implement a named parameter program

--- Functions/named-parameters.py
+++ Functions/named-parameters.py
@@ -117,3 +117,21 @@ def total_marks(physics, chemistry, biology, maths, english):
 
 
 total_marks(chemistry=78, maths=70, english=94, biology=76, physics=89)
+
+
+# Named Parameter Revision
+
+
+def total_marks(physics, chemistry, biology, maths, english):
+
+    print(f"Your marks in physics = {physics}")
+    print(f"Your marks in chemistry = {chemistry}")
+    print(f"Your marks in biology = {biology}")
+    print(f"Your marks in maths = {maths}")
+    print(f"Your marks in english = {english}")
+
+    total = physics + chemistry + biology + maths + english
+    print(f"Your total marks = {total}")
+
+
+total_marks(chemistry=77, maths=60, english=89, biology=92, physics=81)
