35294637a704a05c2b2a50dd388a0534f71ba329
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-09-12 20:36:14 +0530

    Implement a break statment program

--- Loops/break_statement.py
+++ Loops/break_statement.py
@@ -24,3 +24,15 @@ for i in range(1, 798):
     print(i)
     if i == 540:
         break
+
+
+for i in range(2040, 5043):
+    if i == 5008:
+        break
+    print(i)
+
+
+for i in range(545, 654):
+    if i == 530:
+        break
+    print(i)
