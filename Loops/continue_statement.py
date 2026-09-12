61f70b6e56469e4ee9959fd46a783891316de0a6
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-09-12 20:38:01 +0530

    Implement a continue statement prorgam

--- Loops/continue_statement.py
+++ Loops/continue_statement.py
@@ -25,3 +25,14 @@ for i in range(50, 66):
     print("done")
 
 print("Program Finish")
+
+
+# Continue statement Revision
+
+for i in range(1, 31):
+    if i == 22:
+        continue
+    print(i)
+    print("done")
+
+print("Program Finish")
