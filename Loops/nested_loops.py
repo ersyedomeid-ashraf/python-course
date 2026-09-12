45bfd19512cf819c4a52cdd7dc72fdc908fd31db
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-09-12 20:42:11 +0530

    Implement a nested loop program

--- Loops/nested_loops.py
+++ Loops/nested_loops.py
@@ -1,14 +1,21 @@
-# Nested Loops 
+# Nested Loops
 
-for i in range(1,4) :
-    print (i)
-    for j in range(8,12) :
-        print(j) 
+for i in range(1, 4):
+    print(i)
+    for j in range(8, 12):
+        print(j)
 
 
-
-for i in range(1,8) :
+for i in range(1, 8):
     print(i)
 
-for j in range(14,18) :
+for j in range(14, 18):
     print(j)
+
+
+# Nested Loops Revision
+
+for i in range(1, 6):
+    print(i)
+    for j in range(4, 22):
+        print(j)
