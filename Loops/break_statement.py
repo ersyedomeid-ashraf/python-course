467fcdcf8c6ba6cbfb3b5fd32f4cbb86869b4111
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-09-12 20:34:17 +0530

    Implement a break statement program

--- Loops/break_statement.py
+++ Loops/break_statement.py
@@ -1,20 +1,26 @@
-# Break statement 
+# Break statement
 
-for i in range(1,100) :
+for i in range(1, 100):
     print(i)
-    if i == 10 :
-        break 
+    if i == 10:
+        break
 
 
-for i in range(200,5000):
-    if i == 256 :
-        break 
+for i in range(200, 5000):
+    if i == 256:
+        break
     print(i)
 
 
+for i in range(321, 455):
+    if i == 330:
+        break
+    print(i)
 
 
-for i in range(321,455) :
-    if i == 330 :
-        break 
-    print (i)
+# Break statement Revision
+
+for i in range(1, 798):
+    print(i)
+    if i == 540:
+        break
