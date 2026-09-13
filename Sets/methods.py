abae55c68662b4469384c4ddfa90732e91081bf2
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-09-13 18:50:04 +0530

    Implement a sets program

--- Sets/methods.py
+++ Sets/methods.py
@@ -15,3 +15,10 @@ d = set(b)
 
 result = list(c.intersection(d))
 print(result)
+
+
+my_set1 = {44, 73, 56, 67, 78, 89, 29, 45, 66, 78, 75, 43}
+my_set2 = {909, 889, 778, 76, 46, 55}
+
+print(my_set1.union(my_set2))
+print(my_set1.intersection(my_set2))
