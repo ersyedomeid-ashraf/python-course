5a867f51d106efe539927630cd7fe22336999ff0
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-09-14 19:05:07 +0530

    Add a program to print all numbers between those two numbers

--- Questions/104.py
+++ Questions/104.py
@@ -10,3 +10,15 @@ for i in range(1, num + 1):
     fact = fact * i
 
 print(f"Factorial = {fact}")
+
+
+"""
+Write a program to ask two numbers from user. Print all numbers between those two numbers.
+"""
+
+
+start = int(input("Enter starting number = "))
+end = int(input("Enter ending number = "))
+
+for i in range(start, end + 1):
+    print(i)
