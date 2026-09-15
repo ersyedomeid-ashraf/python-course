f59053587fd79fc543faf9cab68c21f262239fd5
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-09-15 21:36:43 +0530

    Add a program to count how many times the given character occurs in the string

--- Questions/107.py
+++ Questions/107.py
@@ -16,3 +16,21 @@ for ch in text:
 
 print("Uppercase characters:", upper)
 print("Lowercase characters:", lower)
+
+
+"""
+WAP to input a string and a character from the user. Count how many 
+times the given character occurs in the string.
+"""
+
+
+text = input("Enter a string: ")
+ch = input("Enter a character: ")
+
+count = 0
+
+for x in text:
+    if x == ch:
+        count += 1
+
+print("Character occurs", count, "times")
