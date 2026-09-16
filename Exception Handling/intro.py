1812cc087db59e52e6e0639416e80d3f05dad41c
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-09-16 23:01:45 +0530

    Add a program of exception handling

---
+++ Exception Handling/intro.py
@@ -0,0 +1,17 @@
+"""
+Exception Handling:- Exception handling is a mechanism used in programming to handle errors
+that occur during the execution of a program.
+"""
+
+try:
+    list = [23, 43, 3, 3, 56, 78, 88]
+
+    print(list[1])
+    print(list[3])
+    print(list[65])
+except:
+    print("Some error occurred")
+
+
+print("Done")
+print("Goodbye")
