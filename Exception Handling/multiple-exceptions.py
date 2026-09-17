6ae78bd7b2997c8b2535ab41c48034ac89a9cc15
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-09-17 17:28:48 +0530

    Add multiple exception handling program

--- Exception Handling/multiple-exceptions.py
+++ Exception Handling/multiple-exceptions.py
@@ -20,3 +20,22 @@ try:
 except ZeroDivisionError:
 
     print("You cannot divide by zero")
+
+
+try:
+    my_list = [2, 5, 6, 7, 88, 0]
+    # print(my_list[78])
+    # print(my_list[0] / my_list[-1])
+    my_list = my_list * "abc"
+
+except IndexError:
+
+    print("Invalid Index")
+
+except ZeroDivisionError:
+
+    print("You cannot divide by zero")
+
+except:
+
+    print("Some error occurred")
