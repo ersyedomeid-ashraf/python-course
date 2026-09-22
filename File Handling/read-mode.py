a069f80d68d48f9b686c9429442a23da6e13a25a
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-09-22 23:24:54 +0530

    Implement a file handling read program

--- File Handling/read-mode.py
+++ File Handling/read-mode.py
@@ -2,3 +2,21 @@ f = open("File Handling/hello.txt", "r")
 x = f.read()
 print(x)
 f.close()
+
+
+# Another one
+
+f = open("File Handling/hello.txt", "r")
+
+print(f.read(6))
+print(f.read(8))
+
+f.close
+
+# Another one
+
+f = open("File Handling/hello.txt", "r")
+
+print(f.readline())
+
+f.close
