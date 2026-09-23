3bb61b65c9d1cb5ca45e3b2dd10e3666e76ae34c
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-09-23 23:04:31 +0530

    Implement a program With Read Keyword program

---
+++ File Handling/read-mode-1.py
@@ -0,0 +1,7 @@
+with open("File Handling/hello.txt", "r") as f:
+
+    x = f.read()
+
+    for ch in x:
+
+        print(ch)
---
+++ File Handling/with-read.py
@@ -0,0 +1,5 @@
+# With Keyword
+
+with open("File Handling/hello.txt", "r") as f:
+
+    print(f.read())
