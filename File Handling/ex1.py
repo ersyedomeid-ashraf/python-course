58dbb9ab8a6b701d2f1dfb82805ea7867845deef
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-09-24 22:09:53 +0530

    Implement a write-mode file handling program

--- File Handling/ex1.py
+++ File Handling/ex1.py
@@ -11,3 +11,20 @@ with open(file_name, "w") as f:
 
         f.write(sentence)
         f.write("\n")
+
+
+# Another one
+
+file_name = input("Enter file name = ")
+file_name = file_name + ".txt"
+
+with open(file_name, "w") as f:
+
+    while True:
+        item = input("Enter item = ")
+
+        if item == "q" or item == "Q":
+            break
+
+        f.write(item)
+        f.write("\n")
