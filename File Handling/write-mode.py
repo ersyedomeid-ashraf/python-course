ca37d88a4328fc69ba798153a000fe88a1003d36
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-09-24 21:12:32 +0530

    Implement a write-mode file handling program

--- File Handling/write-mode.py
+++ File Handling/write-mode.py
@@ -3,3 +3,12 @@
 with open("File Handling/hello.txt", "w") as f:
 
     f.write("Hello World")
+
+    f.write("Teri mushkil na badhaounga chala jaaounga")
+
+
+with open("File Handling/hello1.txt", "w") as f:
+
+    f.write("Ashqq aankhon me chupaaounga chala jaaounga\n")
+    f.write("Apnii dehleez pe kuch der or pada rhnede\n")
+    f.write("Jaise hii hosh me aaounga chala jaaounga\n")
